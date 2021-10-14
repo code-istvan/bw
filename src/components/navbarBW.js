5cdd5d2de9334d96b34d74f7e0ce517ef6f159b3
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-10-14 13:57:13 +0200

    some navbar changes

--- src/components/navbarBW.js
+++ src/components/navbarBW.js
@@ -1,15 +1,11 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Link } from "gatsby"
-// import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import "../sass/components/_navbarbw.scss"
-// import Container from "react-bootstrap/Container"
+import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
-// import Navbar from "react-bootstrap/Navbar"
-// import Nav from "react-bootstrap/Nav"
-// import NavDropdown from "react-bootstrap/NavDropdown"
 import LogoBW from "./LogoBW"
 import LogoBWlong from "./LogoBWlong"
 
@@ -43,7 +39,6 @@ export default function NavbarBW() {
               <LogoBW />
               <LogoBWlong />
             </div>
-
             <button
               type="button"
               className="btn-close btn-close-white text-reset"
@@ -51,14 +46,55 @@ export default function NavbarBW() {
               aria-label="Close"
             ></button>
           </div>
-
-          <div className="offcanvas-body">
+          <Row className="offcanvas-body">
             <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-              <Nav.Item as="li">
-                <Link className="nav-link active" aria-current="page" to="/">
-                  FŐOLDAL
-                </Link>
-              </Nav.Item>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  JÓGAÓRÁK
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <Nav.Item as="li">
+                    <Link className="dropdown-item" to="/orarend">
+                      ÓRAREND
+                    </Link>
+                  </Nav.Item>
+                  <Nav.Item as="li">
+                    <Link className="dropdown-item" to="/arak">
+                      ÁRAK
+                    </Link>
+                  </Nav.Item>
+                  <Nav.Item as="li">
+                    <Link className="dropdown-item" to="/mysore-program">
+                      MYSORE PROGRAM
+                    </Link>
+                  </Nav.Item>
+                  <Nav.Item as="li">
+                    <Link className="dropdown-item" to="/tanfolyam">
+                      TANFOLYAMOK
+                    </Link>
+                  </Nav.Item>
+                  <Nav.Item as="li">
+                    <Link className="dropdown-item" to="/oktatok">
+                      OKTATÓK
+                    </Link>
+                  </Nav.Item>
+                  <Nav.Item as="li">
+                    <Link
+                      className="dropdown-item"
+                      to="/vinyasza-jogairanyzatok"
+                    >
+                      VINYÁSZA JÓGAIRÁNYZATOK
+                    </Link>
+                  </Nav.Item>
+                </ul>
+              </li>
               <li className="nav-item dropdown">
                 <a
                   className="nav-link dropdown-toggle"
@@ -108,53 +144,6 @@ export default function NavbarBW() {
                   </Nav.Item>
                 </ul>
               </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  JÓGAÓRÁK
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <Nav.Item as="li">
-                    <Link className="dropdown-item" to="/orarend">
-                      ÓRAREND
-                    </Link>
-                  </Nav.Item>
-                  <Nav.Item as="li">
-                    <Link className="dropdown-item" to="/arak">
-                      ÁRAK
-                    </Link>
-                  </Nav.Item>
-                  <Nav.Item as="li">
-                    <Link className="dropdown-item" to="/mysore-program">
-                      MYSORE PROGRAM
-                    </Link>
-                  </Nav.Item>
-                  <Nav.Item as="li">
-                    <Link className="dropdown-item" to="/tanfolyam">
-                      TANFOLYAMOK
-                    </Link>
-                  </Nav.Item>
-                  <Nav.Item as="li">
-                    <Link className="dropdown-item" to="/oktatok">
-                      OKTATÓK
-                    </Link>
-                  </Nav.Item>
-                  <Nav.Item as="li">
-                    <Link
-                      className="dropdown-item"
-                      to="/vinyasza-jogairanyzatok"
-                    >
-                      VINYÁSZA JÓGAIRÁNYZATOK
-                    </Link>
-                  </Nav.Item>
-                </ul>
-              </li>
               <li className="nav-item">
                 <a className="nav-link disabled">GALÉRIA</a>
               </li>
@@ -174,120 +163,9 @@ export default function NavbarBW() {
                 </Link>
               </Nav.Item>
             </ul>
-          </div>
+          </Row>
         </div>
       </div>
     </nav>
   )
 }
-
-// export default function NavbarBW() {
-//   return (
-//     <nav className="navbar fixed-top navbar-expand-xxl navbar-dark">
-//       <div className="container-fluid">
-//         <a className="navbar-brand" href="/">
-//           <LogoBW />
-//           <LogoBWlong />
-//         </a>
-//         <button
-//           className="navbar-toggler collapsed"
-//           type="button"
-//           data-bs-toggle="collapse"
-//           data-bs-target="#navbarSupportedContent"
-//           aria-controls="navbarSupportedContent"
-//           aria-expanded="false"
-//           aria-label="Toggle navigation"
-//         >
-//           <span className="toggler-icon top-bar"></span>
-//           <span className="toggler-icon middle-bar"></span>
-//           <span className="toggler-icon bottom-bar"></span>
-//         </button>
-//         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//           <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-//             <Nav.Item as="li">
-//               <Link className="nav-link active" aria-current="page" to="/">
-//                 FŐOLDAL
-//               </Link>
-//             </Nav.Item>
-//             <li className="nav-item dropdown">
-//               <a
-//                 className="nav-link dropdown-toggle"
-//                 href="#"
-//                 id="navbarDropdown"
-//                 role="button"
-//                 data-bs-toggle="dropdown"
-//                 aria-expanded="false"
-//               >
-//                 TUDNIVALÓK
-//               </a>
-//               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-//                 <Nav.Item as="li">
-//                   <Link className="dropdown-item" to="/rolunk">
-//                     RÓLUNK
-//                   </Link>
-//                 </Nav.Item>
-//                 <Nav.Item as="li">
-//                   <Link className="dropdown-item" to="/elso-alkalom">
-//                     ELSŐ ALKALOM
-//                   </Link>
-//                 </Nav.Item>
-//                 <Nav.Item as="li">
-//                   <Link className="dropdown-item" to="/hazirend">
-//                     HÁZIREND
-//                   </Link>
-//                 </Nav.Item>
-//               </ul>
-//             </li>
-//             <li className="nav-item dropdown">
-//               <a
-//                 className="nav-link dropdown-toggle"
-//                 href="#"
-//                 id="navbarDropdown"
-//                 role="button"
-//                 data-bs-toggle="dropdown"
-//                 aria-expanded="false"
-//               >
-//                 JÓGAÓRÁK
-//               </a>
-//               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-//                 <Nav.Item as="li">
-//                   <Link className="dropdown-item" to="/orarend">
-//                     ÓRAREND
-//                   </Link>
-//                 </Nav.Item>
-//                 <Nav.Item as="li">
-//                   <Link className="dropdown-item" to="/arak">
-//                     ÁRAK
-//                   </Link>
-//                 </Nav.Item>
-//                 <Nav.Item as="li">
-//                   <Link className="dropdown-item" to="/mysore-program">
-//                     MYSORE PROGRAM
-//                   </Link>
-//                 </Nav.Item>
-//               </ul>
-//             </li>
-//             <li className="nav-item">
-//               <a className="nav-link disabled">GALÉRIA</a>
-//             </li>
-//             <Nav.Item as="li">
-//               <Link className="nav-link" to="/blog">
-//                 BLOG
-//               </Link>
-//             </Nav.Item>
-//             <Nav.Item as="li">
-//               <Link className="nav-link" to="https://bandha-payments.hu">
-//                 SHOP
-//               </Link>
-//             </Nav.Item>
-//             <Nav.Item as="li">
-//               <Link className="nav-link" to="/kapcsolat">
-//                 KAPCSOLAT
-//               </Link>
-//             </Nav.Item>
-//           </ul>
-//         </div>
-//       </div>
-//     </nav>
-//   )
-// }
