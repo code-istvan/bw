0f4060af7c8e8340ff77d367705e0c8f71534139
code-istvan <63305129+code-istvan@users.noreply.github.com>
2022-01-19 15:07:06 +0100

    map styling

--- src/components/SnazzyGoogleMap.js
+++ src/components/SnazzyGoogleMap.js
@@ -5,7 +5,13 @@ import { Container, Row } from "react-bootstrap"
 const SnazzyGoogleMap = () => (
   <>
     <Container fluid className="map__box">
-            <iframe src="https://snazzymaps.com/embed/360639" frameborder="0" width="100%" height="400px"  title="Google maps"></iframe>
+      <iframe
+        src="https://snazzymaps.com/embed/360639"
+        frameborder="0"
+        width="100%"
+        height="400px"
+        title="Google maps"
+      ></iframe>
     </Container>
   </>
 )
