06ddc86916917a4732f44794ffc3d9ffffea9368
code-istvan <63305129+code-istvan@users.noreply.github.com>
2022-04-20 16:19:59 +0200

    hero text changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,6 +4,7 @@ import "../sass/components/_hero.scss"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Container"
+import Col from "react-bootstrap/Col"
 
 const Hero = () => (
   <>
@@ -38,8 +39,29 @@ const Hero = () => (
       placeholder="blurred"
     />
     <Container>
-      <Row>
-        <h1 className="test">"óh jógi, ne végezz ászanát vinyásza nélkül"</h1>
+      <Row className="hero--text">
+        <p className="hero--font-big">
+          "óh jógi, ne végezz ászanát vinyásza nélkül"
+        </p>
+      </Row>
+      <Row className="hero--text-small">
+        <Col sm={4}>
+          {" "}
+          <StaticImage
+            className="hero_risi"
+            src="../images/risi.jpg"
+            layout="fullWidth"
+            loading="eager"
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="Astanga jóga Mysore"
+            placeholder="blurred"
+          />
+        </Col>
+        <Col sm={8}>
+          <p className="hero--font-small-serif">Jóga korunta</p>
+          <p className="hero--font-small-sans-serif">VÁMANA RISI</p>
+        </Col>
       </Row>
     </Container>
   </>
