3cfeb7ad7b01363539eb38c05c1829042e1087f7
code-istvan <63305129+code-istvan@users.noreply.github.com>
2022-04-22 15:34:24 +0200

    new sets for mobil

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,64 +8,66 @@ import Col from "react-bootstrap/Col"
 
 const Hero = () => (
   <>
-    <StaticImage
-      className="hero_desktop"
-      src="../images/Hero_desktop.jpg"
-      layout="fullWidth"
-      loading="eager"
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="Astanga jóga Mysore"
-      placeholder="blurred"
-    />
-    <StaticImage
-      className="hero_tablet"
-      src="../images/hero_tablet_test.jpg"
-      layout="fullWidth"
-      loading="eager"
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="Astanga jóga Mysore"
-      placeholder="blurred"
-    />
-    <StaticImage
-      className="hero_mobil"
-      src="../images/hero_mobil.jpg"
-      layout="fullWidth"
-      loading="eager"
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="Astanga jóga Mysore"
-      placeholder="blurred"
-    />
-    <Container className="hero--text">
-      <Row>
-        <p className="hero--font-big">
-          "óh jógi, ne végezz ászanát vinyásza nélkül"
-        </p>
-      </Row>
-      <Row className="hero--text-small">
-        <Col sm={4}>
-          {" "}
-          <StaticImage
-            className="hero--risi"
-            src="../images/risi.jpg"
-            layout="fullWidth"
-            loading="eager"
-            quality={95}
-            formats={["AUTO", "WEBP", "AVIF"]}
-            alt="Astanga jóga Mysore"
-            placeholder="blurred"
-          />
-        </Col>
-        <Col sm={8}>
-          <div className="hero--author">
-            <p className="hero--font-small-serif">Jóga korunta</p>
-            <p className="hero--font-small-sans-serif">VÁMANA RISI</p>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <div className="qqq">
+      <StaticImage
+        className="hero_desktop"
+        src="../images/Hero_desktop.jpg"
+        layout="fullWidth"
+        loading="eager"
+        quality={95}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="Astanga jóga Mysore"
+        placeholder="blurred"
+      />
+      <StaticImage
+        className="hero_tablet"
+        src="../images/hero_tablet_test.jpg"
+        layout="fullWidth"
+        loading="eager"
+        quality={95}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="Astanga jóga Mysore"
+        placeholder="blurred"
+      />
+      <StaticImage
+        className="hero_mobil"
+        src="../images/hero_mobil.jpg"
+        layout="fullWidth"
+        loading="eager"
+        quality={95}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="Astanga jóga Mysore"
+        placeholder="blurred"
+      />
+      <Container className="hero--text">
+        <Row>
+          <p className="hero--font-big">
+            "óh jógi, ne végezz ászanát vinyásza nélkül"
+          </p>
+        </Row>
+        <Row className="hero--text-small">
+          <Col sm={4}>
+            {" "}
+            <StaticImage
+              className="hero--risi"
+              src="../images/risi.jpg"
+              layout="fullWidth"
+              loading="eager"
+              quality={95}
+              formats={["AUTO", "WEBP", "AVIF"]}
+              alt="Astanga jóga Mysore"
+              placeholder="blurred"
+            />
+          </Col>
+          <Col sm={8}>
+            <div className="hero--author">
+              <p className="hero--font-small-serif">Jóga korunta</p>
+              <p className="hero--font-small-sans-serif">VÁMANA RISI</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   </>
 )
 
