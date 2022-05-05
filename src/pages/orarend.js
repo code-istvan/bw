354dd318e4e7293fa42d8f16e34e4a83b6a94615
code-istvan <63305129+code-istvan@users.noreply.github.com>
2022-05-05 13:06:41 +0200

    widget test

--- src/pages/orarend.js
+++ src/pages/orarend.js
@@ -5,6 +5,7 @@ import Seo from "../components/seo"
 import { Row, Col } from "react-bootstrap"
 // import { Link } from "gatsby"
 // import "../sass/components/_kapcsolat.scss"
+import { Helmet } from "react-helmet"
 
 export default function Orarend() {
   return (
@@ -13,6 +14,8 @@ export default function Orarend() {
       <Row>
         <Col>
           <h1>ÓRAREND</h1>
+          <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+          <div class="elfsight-app-fb5099a3-5f9f-48b6-944a-44e4eaad58cf"></div>
         </Col>
       </Row>
     </Layout>
