090fc6b1477fc20777d7d4deb7e124640c71150c
code-istvan <63305129+code-istvan@users.noreply.github.com>
2022-05-06 13:53:02 +0200

    widget problem solved

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -8,7 +8,7 @@ import { ElfsightWidget } from "react-elfsight-widget"
 // }
 
 const Calendar = () => (
-  <ElfsightWidget widgetID="elfsight-app-fb5099a3-5f9f-48b6-944a-44e4eaad58cf" />
+  <ElfsightWidget widgetID="fb5099a3-5f9f-48b6-944a-44e4eaad58cf" />
 )
 
 export default Calendar
