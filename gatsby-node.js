2f2cb079d7b986900e8d41951e9c97546d3d2a5e
code-istvan <63305129+code-istvan@users.noreply.github.com>
2022-05-11 07:23:52 +0200

    gatsby-node.js comment changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,16 @@
-// exports.onCreateWebpackConfig = helper => {
-//   const { stage, actions, getConfig } = helper
-//   if (stage === "develop" || stage === "build-javascript") {
-//     const config = getConfig()
-//     const miniCssExtractPlugin = config.plugins.find(
-//       plugin => plugin.constructor.name === "MiniCssExtractPlugin"
-//     )
-//     if (miniCssExtractPlugin) {
-//       miniCssExtractPlugin.options.ignoreOrder = true
-//     }
-//     actions.replaceWebpackConfig(config)
-//   }
-// }
+exports.onCreateWebpackConfig = helper => {
+  const { stage, actions, getConfig } = helper
+  if (stage === "develop" || stage === "build-javascript") {
+    const config = getConfig()
+    const miniCssExtractPlugin = config.plugins.find(
+      plugin => plugin.constructor.name === "MiniCssExtractPlugin"
+    )
+    if (miniCssExtractPlugin) {
+      miniCssExtractPlugin.options.ignoreOrder = true
+    }
+    actions.replaceWebpackConfig(config)
+  }
+}
 
 const { createFilePath } = require(`gatsby-source-filesystem`)
 const path = require(`path`)
