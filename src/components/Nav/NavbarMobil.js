83761de2459d256215c5225fae469e7f17fe41ab
code-istvan <63305129+code-istvan@users.noreply.github.com>
2022-07-29 22:35:05 +0200

    new sass build

--- src/components/Nav/NavbarMobil.js
+++ src/components/Nav/NavbarMobil.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import { useBreakpoint } from "gatsby-plugin-breakpoints"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../../sass/components/_navbarmobil.scss"
 import LogoBW from "./LogoBW"
@@ -8,25 +9,35 @@ import Hamburger from "./Hamburger"
 
 export default function NavbarMobil() {
   const [menuOpen, setMenuOpen] = useState(false)
-
-  const html = document.querySelector("html")
-  const body = document.querySelector("body")
+  const breakpoints = useBreakpoint()
 
   const menuOpenHandler = () => {
     setMenuOpen(!menuOpen)
   }
 
   useEffect(() => {
+    const html = document.querySelector("html")
+    const body = document.querySelector("body")
+    console.log(menuOpen)
     if (menuOpen) {
       html.style.overflow = "hidden"
       body.style.height = "100%"
       body.style.margin = "0"
     } else {
+      console.log("test")
       html.style.overflow = "auto"
       body.style.height = "auto"
       body.style.margin = "inital"
     }
   }, [menuOpen])
+
+  useEffect(() => {
+    console.log(breakpoints)
+    if (!breakpoints.sm) {
+      console.log("test1")
+      setMenuOpen(false)
+    }
+  }, [breakpoints])
 
   return (
     <nav className="navbar-mobil">
