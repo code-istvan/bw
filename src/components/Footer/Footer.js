928e07736a9ad4a70cd43bd22720d95b6181c8d7
code-istvan <63305129+code-istvan@users.noreply.github.com>
2022-08-23 15:12:33 +0200

    footer redisegn

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,88 @@
 import * as React from "react"
-import FooterMobil from "./FooterMobil"
-// import { useBreakpoint } from "gatsby-plugin-breakpoints"
+import Button from "../Button"
+import Icon from "../Icons/Icon"
+import { navigate } from "gatsby"
+import { Helmet } from "react-helmet"
+import "../../sass/components/_footer.scss"
 
 const Footer = () => {
-  // const breakpoints = useBreakpoint()
-
   return (
-    <div>
-      {/* {breakpoints.md ? <FooterMobil /> : <FooterMobil />} */}
-      <FooterMobil />
+    <div className="container footer bg-shades-darkGray mt-3">
+      <Helmet>
+        {" "}
+        <script
+          src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js"
+          defer
+        ></script>
+      </Helmet>
+      <div className="row">
+        <div className="col">
+          <div className="footer__brand">
+            <div className="col">
+              <Icon.BwSymbol />
+            </div>
+            <div className="col">
+              <p>Astanga Jógaiskola Paramaguru Sharath Jois áldásával</p>
+            </div>
+          </div>
+          <div className="footer__navigation">NAVIGATION</div>
+        </div>
+
+        <div className="col">
+          <div className="footer__newsletter">
+            <h3>IRATKOZZ FEL HÍRLEVELÜNKRE</h3>
+            <p>
+              Értesülj a legfrisebb eseményeinkről és egyéb jógás hírekről
+              elsőkézből
+            </p>
+            <Button
+              type="button"
+              buttonStyle="btn--mobil--secondary--outline"
+              buttonSize="btn--small"
+              onClick={() => {
+                navigate("/hirlevel")
+              }}
+            >
+              FELIRATKOZÁS
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="footer__social">
+        <Icon.Facebook url="https://www.facebook.com/bandhaworks" />
+        <Icon.Instagram url="https://www.instagram.com/bandhaworks/" />
+        <Icon.Twitter url="https://twitter.com/IstvnSzalai5/" />
+        <Icon.Soundcloud url="https://soundcloud.com/bandhaworks-hu/" />
+        <Icon.Messenger url="http://m.me/bandhawork" />
+        <Icon.Telegram url="https://t.me/@istvan108" />
+      </div>
+      <p className="footer--designedby">
+        Designed by <br />
+        <a href="https://omworks.hu">OMWORKS YOGA CREATIVES</a>
+      </p>
+      <div className="row">
+        <div className="col">
+          <br />
+          <strong>KAPCSOLAT</strong>
+          <br />
+          <strong>ADATÉDELEM</strong>
+          <br />
+          <strong>SHOP</strong>
+          <br />
+          <strong>TÁPLÁLKOZÁS</strong>
+          <br />
+          <strong>AJÁNLOTT OLVASMÁNYOK</strong>
+          <br /> <br />
+        </div>
+      </div>
+      <div id="wcb" className="carbonbadge wcb-d"></div>
+
+      <p className="footer--designedby">
+        Designed by <br />
+        <a href="https://omworks.hu">OMWORKS YOGA CREATIVES</a>
+      </p>
     </div>
   )
 }
 
 export default Footer
-
-// when the desktop menu is ready
-
-{
-  /* <div className="row">{breakpoints.md ? <NavbarMobil /> : <NavbarDesktop />}</div> */
-}
