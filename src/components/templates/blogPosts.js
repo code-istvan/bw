cf2dcc386d6e72c14792f7cba2dde6027cfc3c31
code-istvan <63305129+code-istvan@users.noreply.github.com>
2022-11-16 14:58:30 +0100

    blog post js changes

--- src/components/templates/blogPosts.js
+++ src/components/templates/blogPosts.js
@@ -17,55 +17,52 @@ const blogPosts = ({ data }) => {
 
   return (
     <LayoutBlog>
-      <div className="container">
-        <Seo
-          title={frontmatter.title}
-          description={frontmatter.description}
-          thumbnail={src}
-        />
-        <div className="container-fluid">
-          <GatsbyImage image={image} alt={frontmatter.title} />
+      <Seo
+        title={frontmatter.title}
+        description={frontmatter.description}
+        thumbnail={src}
+      />
+      <div className="container-fluid">
+        <GatsbyImage image={image} alt={frontmatter.title} />
+      </div>
+      <div className="row">
+        <div className="col">
+          <h2>{frontmatter.title}</h2>
         </div>
-        <div className="row">
-          <div className="col">
-            <h2>{frontmatter.title}</h2>
-          </div>
+      </div>
+      <div className="row">
+        <div className="col blog-post-details">
+          <p>{frontmatter.author}</p>
+          <div className="blog-card-header-separator"></div>
+          <p>{frontmatter.date}</p>
+          <div className="blog-card-header-separator"></div>
+          <div className="tag-button"></div>
+          <p>{timeToRead} perc olvasás</p>
         </div>
-        <div className="row">
-          <div className="col blog-post-details">
-            <p>{frontmatter.author}</p>
-            <div className="blog-card-header-separator"></div>
-            <p>{frontmatter.date}</p>
-            <div className="blog-card-header-separator"></div>
-            <div className="tag-button"></div>
-            <p>{timeToRead} perc olvasás</p>
-          </div>
-        </div>
-        <div className="blog-card-footer-text tag-button">
-          {tags.map(tag => {
-            return (
-              <p key="tag">
-                <Link to={`/tags/${tag}`}>{tag}</Link>
-              </p>
-            )
-          })}
-        </div>
-        {/* <div className="row">
+      </div>
+      <div className="blog-card-footer-text tag-button">
+        {tags.map(tag => {
+          return (
+            <p key="tag">
+              <Link to={`/tags/${tag}`}>{tag}</Link>
+            </p>
+          )
+        })}
+      </div>
+      {/* <div className="row">
           <div className="col">
             <p>{frontmatter.tags}</p>
           </div>
         </div> */}
-        <br />
-        <br />
-        <article>
-          <MDXRenderer>{body}</MDXRenderer>
-        </article>
-        {/* <Button variant="primary" href="/blog"> */}
-        <Button variant="primary" onClick={() => navigate(-1)}>
-          Vissza
-        </Button>
-        <br /> <br />
-      </div>
+      <br />
+      <br />
+      <article>
+        <MDXRenderer>{body}</MDXRenderer>
+      </article>
+      {/* <Button variant="primary" href="/blog"> */}
+      <Button variant="primary" onClick={() => navigate(-1)}>
+        Vissza
+      </Button>
     </LayoutBlog>
   )
 }
