2866bc4cfe084db820e60e5785e7625f221d3b8c
code-istvan <63305129+code-istvan@users.noreply.github.com>
2022-11-21 15:17:46 +0100

    part2

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import Layout from "../components/layouts/layout"
+import Layout from "../components/Layouts/Layout"
 import Seo from "../components/seo"
 
 const NotFoundPage = () => (
--- src/pages/using-typescript.tsx
+++ src/pages/using-typescript.tsx
@@ -2,7 +2,7 @@
 import * as React from "react"
 import { PageProps, Link, graphql } from "gatsby"
 
-import Layout from "../components/layouts/layout"
+import Layout from "../components/Layouts/Layout"
 import Seo from "../components/seo"
 
 type DataProps = {
