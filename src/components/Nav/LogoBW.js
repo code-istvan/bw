1f0b9682ea6e70e1067df277523e1ada3cc82258
code-istvan <63305129+code-istvan@users.noreply.github.com>
2022-12-13 14:10:45 +0100

    menubar logo interaction

--- src/components/Nav/LogoBW.js
+++ src/components/Nav/LogoBW.js
@@ -8,7 +8,7 @@ function LogoBW() {
       height="40"
       viewBox="0 0 40 40"
       xmlns="http://www.w3.org/2000/svg"
-      className="logo-margin-left"
+      className="logo-margin-left logo-round"
     >
       <path
         id="logo"
