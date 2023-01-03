1c575fa1793a40156d6b188c67e8629ff1965856
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-01-03 22:02:22 +0100

    events card design

--- src/components/EventsRoll.js
+++ src/components/EventsRoll.js
@@ -2,35 +2,48 @@ import React from "react"
 import { useEventsRoll } from "../hooks/useEventsRollQuery"
 import Button from "../components/Buttons/Button"
 import { navigate } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 import "../sass/components/_eventsroll.scss"
 
 export default function EventsRoll() {
   let events = useEventsRoll()
 
-  return events.map(({ title, date, day, Shortdescription }) => {
-    return (
-      <div className="event-card">
-        <div className="event-card-content-wrapper">
-          <div className="event-card-header">
-            <h3>{title}</h3>
-            <p>
-              Kezdés: {date} {day}
-            </p>
-          </div>
-          <div className="event-card-body">
-            <p>{Shortdescription}</p>
-          </div>
-          <div className="event-card-footer">
-            <Button
-              type="button"
-              buttonStyle="btn--third--naked-orange"
-              onClick={() => {
-                navigate("/mysore-program")
-              }}
-            >
-              Részletek
-            </Button>
-            {/* <Button
+  return events.map(
+    ({ title, date, day, Shortdescription, teacher, teacherimage }) => {
+      // console.log("ezaz", teacherimage.childrenImageSharp[0].fixed)
+
+      return (
+        <div className="event-card" key={date}>
+          <div className="event-card-content-wrapper">
+            <div className="event-card-header">
+              <h3>{title}</h3>
+              <div className="event-card-header-secondline">
+                <GatsbyImage
+                  image={teacherimage.childrenImageSharp[0].fixed}
+                  alt={day}
+                  className="avatar-image"
+                />
+                <p>{teacher}</p>
+                <div className="blog-card-header-separator"></div>
+                <p>
+                  {date} {day}
+                </p>
+              </div>
+            </div>
+            <div className="event-card-body">
+              <p>{Shortdescription}</p>
+            </div>
+            <div className="event-card-footer">
+              <Button
+                type="button"
+                buttonStyle="btn--third--naked-orange"
+                onClick={() => {
+                  navigate("/mysore-program")
+                }}
+              >
+                Részletek
+              </Button>
+              {/* <Button
               type="button"
               buttonStyle="btn--third--naked-gray"
               onClick={() => {
@@ -39,9 +52,10 @@ export default function EventsRoll() {
             >
               Jelentkezés
             </Button> */}
+            </div>
           </div>
         </div>
-      </div>
-    )
-  })
+      )
+    }
+  )
 }
