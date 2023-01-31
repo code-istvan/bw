b8aad0a6ef8335535da3ee95a369a7089fc91204
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-01-31 16:39:37 +0100

    cms test

--- src/components/ScheduleRoll.js
+++ src/components/ScheduleRoll.js
@@ -7,33 +7,17 @@ import { element } from "prop-types"
 export default function ScheduleRoll() {
   let schedule = useSchedule()
   const classes = useClasses()
-  const getClassData = currentClass => {
-    const classData = classes.find(item => item.class === currentClass)
-    return classData
-  }
+
   console.log(schedule)
+  console.log(classes)
   return (
     <div className="row">
       {schedule.map(
         ({ class1, teacher1, time1, date, class2, teacher2, time2 }) => {
-          let currentClass
-
-          if (class1) {
-            currentClass = getClassData(class1)
-          }
-          if (class2) {
-            currentClass = getClassData(class2)
-          }
-          console.log(currentClass)
-          // const currentClass = classes.find(
-          //   class1 => class1.shortdescription === classData1
+          // const classShortdescription = classes.find(
+          //   classid => classid.shortdescription === classShortdescription
           // )
-
-          // const mergedClasses = {...class1, ...class2, class3}
-
-          // const currentTeacher = teachers.find(
-          //   teacher => teacher.name === teacherName
-          // )
+          // console.log(classShortdescription)
 
           return (
             <>
@@ -57,3 +41,64 @@ export default function ScheduleRoll() {
     </div>
   )
 }
+
+//Ferran's code
+// import React from "react"
+// import { useSchedule } from "../hooks/useScheduleQuery"
+// import { useClasses } from "../hooks/useClassesQuery"
+// import { useTeachers } from "../hooks/useTeachersQuery"
+// import { element } from "prop-types"
+
+// export default function ScheduleRoll() {
+//   let schedule = useSchedule()
+//   const classes = useClasses()
+//   const getClassData = currentClass => {
+//     const classData = classes.find(item => item.class === currentClass)
+//     return classData
+//   }
+//   console.log(schedule)
+//   return (
+//     <div className="row">
+//       {schedule.map(
+//         ({ class1, teacher1, time1, date, class2, teacher2, time2 }) => {
+//           let currentClass
+
+//           if (class1) {
+//             currentClass = getClassData(class1)
+//           }
+//           if (class2) {
+//             currentClass = getClassData(class2)
+//           }
+//           console.log(currentClass)
+//           // const currentClass = classes.find(
+//           //   class1 => class1.shortdescription === classData1
+//           // )
+
+//           // const mergedClasses = {...class1, ...class2, class3}
+
+//           // const currentTeacher = teachers.find(
+//           //   teacher => teacher.name === teacherName
+//           // )
+
+//           return (
+//             <>
+//               <div className="row">
+//                 <div className="col">{date}</div>
+//               </div>
+//               <div className="row">
+//                 <div className="col">
+//                   {time1} {class1} {teacher1}
+//                 </div>
+//               </div>
+//               <div className="row">
+//                 <div className="col">
+//                   {time2} {class2} {teacher2}
+//                 </div>
+//               </div>
+//             </>
+//           )
+//         }
+//       )}
+//     </div>
+//   )
+// }
