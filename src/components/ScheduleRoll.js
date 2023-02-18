ab291d6e056d67fcac34d4a124b04f7f5a8cadbf
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-02-18 21:59:53 +0100

    some schedule changes

--- src/components/ScheduleRoll.js
+++ src/components/ScheduleRoll.js
@@ -70,53 +70,23 @@ export default function ScheduleRoll() {
                       </div>
                     </div>
                   </div>
-                  <div className="schedule-card">
-                    <div className="schedule-main-row">
-                      <div className="schedule-class">
-                        <p>{time1}</p>
-                        <div className="schedule-class-second-line">
-                          <h3>{class1}</h3>
-                          <div className="vertical-separator" />
-                          <CustomLink
-                            link={teacherlink1}
-                            classNames="body link-decoration-remove clr-brand-orange schedule-teacher"
-                            title={teacher1}
-                          />
-                        </div>
-                        <div className="schedule-details">
-                          <Accordion
-                            content={description1}
-                            text="Részletek"
-                            className="clr-shades-white span"
-                          />
-                        </div>
-                      </div>
-                      <div className="schedule-buy-ticket link-decoration-remove">
-                        <CustomLink
-                          link={signup1}
-                          classNames="body clr-brand-orange"
-                          title="Vásárlás"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className={class2 == "null" ? "hide-class" : ""}>
+                  <div className="card-wrapper">
                     <div className="schedule-card">
                       <div className="schedule-main-row">
                         <div className="schedule-class">
-                          <p>{time2}</p>
+                          <p>{time1}</p>
                           <div className="schedule-class-second-line">
-                            <h3>{class2}</h3>
+                            <h3>{class1}</h3>
                             <div className="vertical-separator" />
                             <CustomLink
-                              link={teacherlink2}
+                              link={teacherlink1}
                               classNames="body link-decoration-remove clr-brand-orange schedule-teacher"
-                              title={teacher2}
+                              title={teacher1}
                             />
                           </div>
                           <div className="schedule-details">
                             <Accordion
-                              content={description2}
+                              content={description1}
                               text="Részletek"
                               className="clr-shades-white span"
                             />
@@ -124,16 +94,55 @@ export default function ScheduleRoll() {
                         </div>
                         <div className="schedule-buy-ticket link-decoration-remove">
                           <CustomLink
-                            link={signup2}
+                            link={signup1}
                             classNames="body clr-brand-orange"
                             title="Vásárlás"
                           />
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div className={class3 == "null" ? "hide-class" : ""}>
-                    <div className="schedule-card">
+
+                    <div
+                      className={
+                        class2 === "null" ? "hide-class" : "schedule-card"
+                      }
+                    >
+                      <div>
+                        <div className="schedule-main-row">
+                          <div className="schedule-class">
+                            <p>{time2}</p>
+                            <div className="schedule-class-second-line">
+                              <h3>{class2}</h3>
+                              <div className="vertical-separator" />
+                              <CustomLink
+                                link={teacherlink2}
+                                classNames="body link-decoration-remove clr-brand-orange schedule-teacher"
+                                title={teacher2}
+                              />
+                            </div>
+                            <div className="schedule-details">
+                              <Accordion
+                                content={description2}
+                                text="Részletek"
+                                className="clr-shades-white span"
+                              />
+                            </div>
+                          </div>
+                          <div className="schedule-buy-ticket link-decoration-remove">
+                            <CustomLink
+                              link={signup2}
+                              classNames="body clr-brand-orange"
+                              title="Vásárlás"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      className={
+                        class3 === "null" ? "hide-class" : "schedule-card"
+                      }
+                    >
                       <div className="schedule-main-row">
                         <div className="schedule-class">
                           <p>{time3}</p>
@@ -163,9 +172,11 @@ export default function ScheduleRoll() {
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div className={class4 == "null" ? "hide-class" : ""}>
-                    <div className="schedule-card">
+                    <div
+                      className={
+                        class4 === "null" ? "hide-class" : "schedule-card"
+                      }
+                    >
                       <div className="schedule-main-row">
                         <div className="schedule-class">
                           <p>{time4}</p>
