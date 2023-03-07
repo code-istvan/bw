80c14d64b8dbfc90f0ca9f0153bede2d974e6a0d
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-03-07 12:40:32 +0100

    bugfix orarend

--- src/components/templates/orarend.js
+++ src/components/templates/orarend.js
@@ -75,43 +75,115 @@ const Orarend = ({ pageContext, data }) => {
               .toUpperCase()
 
             return (
-              <>
-                <div className="schedule-wrapper">
-                  <div className="bg-shades-darkerGray container-fluid">
-                    <div className="container">
-                      <div className="schedule-date-line clr-shades-gray tight--desktop--container">
-                        <div>
-                          <p>{dayName}</p>
-                          {fullmoon && <Icons.Fullmoon url="/holdnapok" />}
-                          {newmoon && <Icons.Newmoon url="/holdnapok" />}
-                          {ekadashi && (
-                            <CustomLink
-                              link="/holdnapok"
-                              title="ekadashi"
-                              classNames="body link-decoration-remove clr-brand-orange schedule-teacher"
-                            />
-                          )}
+              <div className="schedule-wrapper">
+                <div className="bg-shades-darkerGray container-fluid">
+                  <div className="container">
+                    <div className="schedule-date-line clr-shades-gray tight--desktop--container">
+                      <div>
+                        <p>{dayName}</p>
+                        {fullmoon && <Icons.Fullmoon url="/holdnapok" />}
+                        {newmoon && <Icons.Newmoon url="/holdnapok" />}
+                        {ekadashi && (
+                          <CustomLink
+                            link="/holdnapok"
+                            title="ekadashi"
+                            classNames="body link-decoration-remove clr-brand-orange schedule-teacher"
+                          />
+                        )}
+                      </div>
+                      <p>{monthDay}</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="card-wrapper">
+                  <div className="schedule-card">
+                    <div className="schedule-main-row">
+                      <div className="schedule-class">
+                        <p>{time1}</p>
+                        <div className="schedule-class-second-line">
+                          <h3>{class1}</h3>
+                          <CustomLink
+                            link={teacherlink1}
+                            classNames="body link-decoration-remove clr-brand-orange schedule-teacher"
+                            title={teacher1}
+                          />
                         </div>
-                        <p>{monthDay}</p>
+                        <div className="schedule-details">
+                          <Accordion
+                            content={description1}
+                            text="Részletek"
+                            className="clr-shades-white span"
+                          />
+                        </div>
+                      </div>
+                      <div className="schedule-buy-ticket link-decoration-remove">
+                        <CustomLink
+                          link={signup1}
+                          classNames="body clr-brand-orange"
+                          title="Vásárlás"
+                        />
                       </div>
                     </div>
                   </div>
-                  <div className="card-wrapper">
-                    <div className="schedule-card">
+                  {class2 !== "null" && (
+                    <div
+                      className={
+                        class2 === "null" ? "hide-class" : "schedule-card"
+                      }
+                    >
+                      <div>
+                        <div className="schedule-main-row">
+                          <div className="schedule-class">
+                            <p>{time2}</p>
+                            <div className="schedule-class-second-line">
+                              <h3>{class2}</h3>
+                              <div className="vertical-separator" />
+                              <CustomLink
+                                link={teacherlink2}
+                                classNames="body link-decoration-remove clr-brand-orange schedule-teacher"
+                                title={teacher2}
+                              />
+                            </div>
+                            <div className="schedule-details">
+                              <Accordion
+                                content={description2}
+                                text="Részletek"
+                                className="clr-shades-white span"
+                              />
+                            </div>
+                          </div>
+                          <div className="schedule-buy-ticket link-decoration-remove">
+                            <CustomLink
+                              link={signup2}
+                              classNames="body clr-brand-orange"
+                              title="Vásárlás"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                  {class3 !== "null" && (
+                    <div
+                      className={
+                        class3 === "null" ? "hide-class" : "schedule-card"
+                      }
+                    >
                       <div className="schedule-main-row">
                         <div className="schedule-class">
-                          <p>{time1}</p>
+                          <p>{time3}</p>
                           <div className="schedule-class-second-line">
-                            <h3>{class1}</h3>
+                            <h3>{class3}</h3>
+                            <div className="vertical-separator" />
                             <CustomLink
-                              link={teacherlink1}
+                              link={teacherlink3}
                               classNames="body link-decoration-remove clr-brand-orange schedule-teacher"
-                              title={teacher1}
+                              title={teacher3}
                             />
                           </div>
                           <div className="schedule-details">
                             <Accordion
-                              content={description1}
+                              content={description3}
                               text="Részletek"
                               className="clr-shades-white span"
                             />
@@ -119,122 +191,48 @@ const Orarend = ({ pageContext, data }) => {
                         </div>
                         <div className="schedule-buy-ticket link-decoration-remove">
                           <CustomLink
-                            link={signup1}
+                            link={signup3}
                             classNames="body clr-brand-orange"
                             title="Vásárlás"
                           />
                         </div>
                       </div>
                     </div>
-                    {class2 !== "null" && (
-                      <div
-                        className={
-                          class2 === "null" ? "hide-class" : "schedule-card"
-                        }
-                      >
-                        <div>
-                          <div className="schedule-main-row">
-                            <div className="schedule-class">
-                              <p>{time2}</p>
-                              <div className="schedule-class-second-line">
-                                <h3>{class2}</h3>
-                                <div className="vertical-separator" />
-                                <CustomLink
-                                  link={teacherlink2}
-                                  classNames="body link-decoration-remove clr-brand-orange schedule-teacher"
-                                  title={teacher2}
-                                />
-                              </div>
-                              <div className="schedule-details">
-                                <Accordion
-                                  content={description2}
-                                  text="Részletek"
-                                  className="clr-shades-white span"
-                                />
-                              </div>
-                            </div>
-                            <div className="schedule-buy-ticket link-decoration-remove">
-                              <CustomLink
-                                link={signup2}
-                                classNames="body clr-brand-orange"
-                                title="Vásárlás"
-                              />
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    )}
-                    {class3 !== "null" && (
-                      <div
-                        className={
-                          class3 === "null" ? "hide-class" : "schedule-card"
-                        }
-                      >
-                        <div className="schedule-main-row">
-                          <div className="schedule-class">
-                            <p>{time3}</p>
-                            <div className="schedule-class-second-line">
-                              <h3>{class3}</h3>
-                              <div className="vertical-separator" />
-                              <CustomLink
-                                link={teacherlink3}
-                                classNames="body link-decoration-remove clr-brand-orange schedule-teacher"
-                                title={teacher3}
-                              />
-                            </div>
-                            <div className="schedule-details">
-                              <Accordion
-                                content={description3}
-                                text="Részletek"
-                                className="clr-shades-white span"
-                              />
-                            </div>
-                          </div>
-                          <div className="schedule-buy-ticket link-decoration-remove">
+                  )}
+                  {class4 !== "null" && (
+                    <div className="schedule-card">
+                      <div className="schedule-main-row">
+                        <div className="schedule-class">
+                          <p>{time4}</p>
+                          <div className="schedule-class-second-line">
+                            <h3>{class4}</h3>
+                            <div className="vertical-separator" />
                             <CustomLink
-                              link={signup3}
-                              classNames="body clr-brand-orange"
-                              title="Vásárlás"
+                              link={teacherlink4}
+                              classNames="body link-decoration-remove clr-brand-orange schedule-teacher"
+                              title={teacher4}
+                            />
+                          </div>
+                          <div className="schedule-details">
+                            <Accordion
+                              content={description4}
+                              text="Részletek"
+                              className="clr-shades-white span"
                             />
                           </div>
                         </div>
-                      </div>
-                    )}
-                    {class4 !== "null" && (
-                      <div className="schedule-card">
-                        <div className="schedule-main-row">
-                          <div className="schedule-class">
-                            <p>{time4}</p>
-                            <div className="schedule-class-second-line">
-                              <h3>{class4}</h3>
-                              <div className="vertical-separator" />
-                              <CustomLink
-                                link={teacherlink4}
-                                classNames="body link-decoration-remove clr-brand-orange schedule-teacher"
-                                title={teacher4}
-                              />
-                            </div>
-                            <div className="schedule-details">
-                              <Accordion
-                                content={description4}
-                                text="Részletek"
-                                className="clr-shades-white span"
-                              />
-                            </div>
-                          </div>
-                          <div className="schedule-buy-ticket link-decoration-remove">
-                            <CustomLink
-                              link={signup4}
-                              classNames="body clr-brand-orange"
-                              title="Vásárlás"
-                            />
-                          </div>
+                        <div className="schedule-buy-ticket link-decoration-remove">
+                          <CustomLink
+                            link={signup4}
+                            classNames="body clr-brand-orange"
+                            title="Vásárlás"
+                          />
                         </div>
                       </div>
-                    )}
-                  </div>
+                    </div>
+                  )}
                 </div>
-              </>
+              </div>
             )
           }
         })}
