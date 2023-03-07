2d16d48598395184984be21fffc9a99c98669b8c
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-03-07 14:52:08 +0100

    bug if broken links

---
+++ src/pages/galeria.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import Layout from "../components/Layouts/Layout"
+import Seo from "../components/seo"
+// import { Link } from "gatsby"
+// import "../sass/components/_kapcsolat.scss"
+
+export default function Galeria() {
+  return (
+    <Layout>
+      <Seo title="Galéria" />
+      <div className="row">
+        <h1>GALÉRIA</h1>
+      </div>
+    </Layout>
+  )
+}
