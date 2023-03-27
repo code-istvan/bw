2784f7a5df08eb289ee4e6a74c93346c46c25e83
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-03-27 16:16:33 +0200

    bugfix2 layout

--- src/components/templates/orarend.js
+++ src/components/templates/orarend.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import { CustomLink } from "../CustomLink"
-import Layout from "../../components/Layouts/Layout"
+import LayoutBlog from "../../components/Layouts/LayoutBlog"
 import Seo from "../../components/seo"
 import Accordion from "../Accordion"
 import Icons from "../Icons/Icons"
@@ -21,7 +21,7 @@ const Orarend = ({ pageContext, data }) => {
   })
 
   return (
-    <Layout>
+    <LayoutBlog>
       <Seo title="ÓRAREND" />
       <div className="row mt-80px mb-20px">
         <h1>ÓRAREND</h1>
@@ -258,7 +258,7 @@ const Orarend = ({ pageContext, data }) => {
           )}
         </div>
       </div>
-    </Layout>
+    </LayoutBlog>
   )
 }
 
