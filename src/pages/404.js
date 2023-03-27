b48ea8b9654b390cdacdae3739deba3af5dc6d86
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-03-27 16:44:26 +0200

    bugfix3

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,13 @@
 import * as React from "react"
-import Layout from "../components/Layouts/Layout"
+import LayoutBlog from "../components/Layouts/LayoutBlog"
 import Seo from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
+  <LayoutBlog>
     <Seo title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  </LayoutBlog>
 )
 
 export default NotFoundPage
