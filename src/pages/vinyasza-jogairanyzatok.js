2e5f2ecd48f3982a7b420b9d42ba162c915eab1a
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-03-27 15:48:39 +0200

    bugfix layout

--- src/pages/vinyasza-jogairanyzatok.js
+++ src/pages/vinyasza-jogairanyzatok.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import LayoutBlog from "../components/Layouts/Layout"
+import LayoutBlog from "../components/Layouts/LayoutBlog"
 import Seo from "../components/seo"
 // import { Link } from "gatsby"
 // import "../sass/components/_kapcsolat.scss"
