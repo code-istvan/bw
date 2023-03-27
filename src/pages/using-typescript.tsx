cede657e822fdd95d1ae896b07760d55e79ad9b5
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-03-27 16:25:37 +0200

    bugfix3

--- src/pages/using-typescript.tsx
+++ src/pages/using-typescript.tsx
@@ -2,7 +2,7 @@
 import * as React from "react"
 import { PageProps, Link, graphql } from "gatsby"
 
-import Layout from "../components/Layouts/Layout"
+import LayoutBlog from "../components/Layouts/LayoutBlog"
 import Seo from "../components/seo"
 
 type DataProps = {
@@ -12,7 +12,7 @@ type DataProps = {
 }
 
 const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
-  <Layout>
+  <LayoutBlog>
     <Seo title="Using TypeScript" />
     <h1>Gatsby supports TypeScript by default!</h1>
     <p>
@@ -36,7 +36,7 @@ const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
       .
     </p>
     <Link to="/">Go back to the homepage</Link>
-  </Layout>
+  </LayoutBlog>
 )
 
 export default UsingTypescript
