629e23a811055735a813f3f8df5c2795d92199e0
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-04-11 22:46:42 +0200

    moondays copywrite

--- src/pages/holdnapok.js
+++ src/pages/holdnapok.js
@@ -73,91 +73,106 @@ export default function Holdnapok() {
             <p>
               Sokféle ötlet szokott felmerülni azzal kapcsolatban, hogy Pattabhi
               Jois miért nem tartott órákat teli- és újholdkor (röviden
-              holdnapokon). Pedig a magyarázat egyszerű. Amint tudod, a
-              Maharádzsa Pathasálája (a Szanszkrit Főiskola) szünetet tartott
-              minden holdnapon, illetve az azokat megelőző és követő napokon. A
-              tanulók otthon folytatták a tanulmányaikat, de nem kaptak új
-              leckéket. Ennek egyik oka az, hogy az amavaszja és a púrnima
-              napokon bizonyos rituálékat kellett a tanároknak és a diákoknak
-              végrehajtaniuk, akik mindannyian brahminok voltak – például a
-              pitri tarpanát, amelyet amavaszján végeztek, és a rituális fürdőt
-              a holdnapokat követő napon. Ezek a szertartások meglehetősen
-              időigényesek. Ezen kívül, bár én soha nem találtam erre utalást,
-              de Pattabhi Jois gyakran idézte nekünk, illetve a mysore-i
-              Bhagavad Gítá tanárom, Naranacharya Professzor is említette, hogy
-              ha egy tanár holdnapon újat tanít, akkor csökken a tudása, a
-              megelőző és követő napokon pedig a diák tudása csökken.
+              holdnapokon). Pedig a magyarázat egyszerű.
+            </p>
+            <p>
+              A Maharádzsa Pathasálája (a Szanszkrit Főiskola) szünetet tartott
+              minden holdnapon, valamint az azokat megelőző és követő napokon.
+              Ilyenkor a tanulók otthon folytatták a tanulmányaikat, de új
+              leckéket nem kaptak. Ennek egyik oka az, hogy az amavaszja
+              (újhold) és a púrnima (telihold) napokon bizonyos rituálékat
+              kellett a tanároknak és a diákoknak végrehajtaniuk, akik
+              mindannyian brahminok voltak. Ilyen rituálé például a pitri
+              tarpana újhold napján, vagy a holdnapokat követő napon végzendő
+              rituális fürdő. Ezek a szertartások meglehetősen időigényesek.
+              Ezen kívül – bár én soha nem találtam erre utalást – Pattabhi Jois
+              gyakran idézte (és a mysore-i Bhagavad Gíta tanárom, Naranacharya
+              Professzor is említette), hogy ha egy tanár holdnapokon újat
+              tanít, akkor csökken a tudása. A holdnapot megelőző és követő
+              napokon pedig a diák tudása csökken.
             </p>
             <br />
             <p>
               Amikor beszéltem Pattabhi Jois asztrológusával a Guruji könyvben
-              lévő interjú készítésekor, akkor ő is egyet értett azzal, hogy ez
-              a szokás azzal a gondolattal függ össze, hogy “amint fent úgy
-              lent”: a védikus hagyomány szerint az elménk olyan, mint a hold:
-              növekszik, csökken és ugyanezen ciklusok szerint tárolja az
-              információt, mivel a Hold hatással van a Föld gravitációjára.
+              lévő interjú készítésekor, ő is egyetértett azzal, hogy ez a
+              szokás azt a védikus elképzelést tükrözi, miszerint az elménk
+              olyan, mint a hold. „Amint fent, úgy lent”: növekszik, csökken, és
+              ugyanazon ciklusok szerint tárolja az információt, ahogy a Hold is
+              változik, mivel a Hold hatással van a Föld gravitációjára.
             </p>
             <br />
             <p>
               Mivel Pattabhi Jois a Maharádzsa Pathasálájának tanulója volt,
-              majd a főiskola Jógaprofesszora lett 1937-1973 között, ezért a
+              majd a főiskola jógaprofesszora lett 1937 és 1973 között, ezért a
               tanítási szünet ezeken a napokon szokásává vált. Mivel ő a jóga
               gyakorlását védikus eredetűnek tartotta, és úgy gondolta, hogy a
               pránájáma és az ászana az Upanisadok megértéséhez vezető út
               kapuja, ezért a jóga tanítására is ugyanazokat a szabályokat
-              vonatkoztatta, mint a Védák oktatására. Továbbá azt is mondta,
-              hogy a holdnapokon könnyebb megsérülni, és a sérülések lassabban
-              gyógyulnak. Személy szerint ezt sosem tudtam alátámasztani a
-              dzsjótis alapján; talán ez is olyasvalami, amit az édesapjától
-              tanult, aki kiváló asztrológus volt.
+              vonatkoztatta, mint a Védák oktatására.
             </p>
+            <br />
             <p>
-              Pattabhi maga is értett az asztrológiához – a neve, Jois a
-              dzsjótis (asztrológus) szó dél-indiai torzítása, tehát az
-              asztrológia a családi hagyományuk része volt. Azért mondom el
-              mindezt, hogy megértessem, hogy Pattabhi Joisnak voltak bizonyos
-              szokásai 14 évesen. Hogy miért voltak ezek a szokásai az is
-              érdekes, és bár lehet, hogy mi nem vagyunk brahminok, sőt még
-              indiaiak sem, de mivel a tanítványai vagyunk, ezért jó, ha
-              megértjük miért tett bizonos dolgokat, és elfogadjuk azt, hogy ha
-              neki fontos volt követni őket, akkor ránk is vonatkoznak. De nem
-              érdemes túl nagy ügyet csinálni belőlük, vagy mindenféle
+              Továbbá azt is mondta, hogy a holdnapokon könnyebb megsérülni, és
+              a sérülések maguk is lassabban gyógyulnak. Személy szerint ezt
+              sosem tudtam alátámasztani a dzsjótis (védikus asztrológia)
+              alapján. Talán ez is olyasvalami, amit az édesapjától tanult, aki
+              kiváló asztrológus volt. Patthabhi maga is értett az
+              asztrológiához – a neve, Jois, a dzsjótis (asztrológus) szó
+              dél-indiai torzítása, tehát az asztrológia a családi hagyományuk
+              része volt.
+            </p>
+            <br />
+            <p>
+              Azért mondom el mindezt, hogy lássuk, Patthabhi Joisnak voltak
+              bizonyos szokásai 14 évesen. Hogy miért voltak ezek a szokásai, az
+              önmagában is érdekes. És bár lehet, hogy mi nem vagyunk brahminok,
+              sőt még indiaiak sem, de mivel a tanítványai vagyunk, jó, ha
+              megértjük miért tett bizonyos dolgokat, és elfogadjuk azt, hogy ha
+              neki fontos volt követni őket, akkor ránk is vonatkoznak.
+            </p>
+            <br />
+
+            <p>
+              De nem érdemes túl nagy ügyet csinálni belőlük, vagy mindenféle
               fantasztikus sztorit költeni köréjük! Itt van például egy vicces
-              történet, ami jól jellemzi, hogy mi (például az astanga jóga
-              gyakorlói!) nem szánjuk rá az időt, hogy egyszerű dolgokra fényt
-              derítsünk racionális elmével: Egy szent tudós minden este egy fa
-              alatt tartott Bhagavad Gítá leckéket egy falu mellett. Volt egy
+              történet, ami jól illusztrállja, milyen az, amikor vakon követünk
+              szokásokat, és nem szánjuk rá az időt, hogy racionális elmével
+              utánajárjunk az értelmüknek. Egy szent tudós minden este egy fa
+              alatt tartott Bhagavad Gíta leckéket egy falu mellett. Volt egy
               macskája, aki néha beugrott a tömegbe, és megzavarta a
               hallgatóságot. Ezért a bölcs mindig kikötötte őt a fához az órák
               alatt. Évek múltán a bölcs eltávozott az élők soraiból. Az egyik
-              tanítványa folytatta az esti Bhagavad Gítá órákat a fa alatt, és ő
-              is kikötötte a macskát a fához. A macska is megörgedett és meghalt
-              idővel, és a tanítvány szerzett egy új macskát. Három generáció
-              után az egyik tanítvány írt egy könyvet arról a szent tradícióról,
-              hogy Bhagavad Gítá leckék alatt egy macskát kell a fához
-              kötni.Tehát mindezek után azt gondolom, hogy a Pattabhi Jois és a
-              módszere iránt érzett tiszteletből jó, ha a tanítványai követik a
-              holdnapokra vonatkozó szabályokat, ha lehetőságük van rá.
-              Mindennek az a célja, hogy kapcsolódjunk a tanítói láncolathoz,
-              illetve alázatot, figyelmességet és egyfajta fegyelmet ébresszen a
-              tanítványokban. (Nagyon valószínű, hogy) nem fogunk pokolra jutni,
-              ha ezeken a napokon gyakorlunk; Pattabhi Jois lánya, Saraswati
-              (aki a Szanszrkit Főiskolán az első és egyetlen női jógatanítványa
-              is volt egyben) hétfőtől péntekig tanított, és a hétvégék voltak a
-              pihenőnapok, illetve hozzátette, hogy holdnapokon nem ad új
-              ászanát. Tisztában volt vele, hogy a tanítványai nem gyakorolnak a
-              hét minden napján, de akik mégis ezt teszik, azok testének jól jön
-              egy extra pihenőnap.
+              tanítványa folytatta az esti Bhagavad Gíta órákat a fa alatt, és ő
+              is kikötötte a macskát a fához. A macska is megöregedett és
+              meghalt idővel, és a tanítvány szerzett egy új macskát. Három
+              generáció elteltével az egyik tanítvány írt egy könyvet arról a
+              szent tradícióról, hogy Bhagavad Gíta leckék alatt egy macskát
+              kell a fához kötni.
+            </p>
+            <p>
+              Mindezek után azt gondolom, hogy Pattabhi Jois és a módszere iránt
+              érzett tiszteletből jó, ha a tanítványai követik a holdnapokra
+              vonatkozó szabályokat, ha lehetőségük van rá. Mindennek az a
+              célja, hogy kapcsolódjunk a tanítói láncolathoz, illetve alázatot,
+              figyelmességet és egyfajta fegyelmet ébresszen a tanítványokban.
+              (Nagyon valószínű, hogy) nem fogunk pokolra jutni, ha ezeken a
+              napokon gyakorlunk. Pattabhi Jois lánya, Saraswathi (aki a
+              Szanszkrit Főiskola első és egyetlen női jógatanítványa is volt
+              egyben) például hétfőtől péntekig tanított, és a hétvégék voltak a
+              pihenőnapok, illetve holdnapokon nem adott új ászanát. Tisztában
+              volt vele, hogy a tanítványai nem gyakorolnak a hét minden napján,
+              de akik mégis ezt teszik, azok testének jól jön egy extra
+              pihenőnap.
             </p>
             <br />
             <p>
               Van egy sajátos bája annak, ha meghódolunk egy láncolat előtt, és
-              ez hatással van a személyiségünkre is, tehát miért ne próbálnánk
-              meg? Én nem hiszem, hogy minden jógagyakorlónak szigorúan
-              pihenőnapot kellene tartania ezeken a napokon – mindenki kövesse a
-              saját tanárát, és remélhetőleg, ha magasabb elvekhez hangoljuk az
+              ez hatással van a személyiségünkre, tehát miért ne próbálnánk meg?
+              Nem hiszem, hogy minden jógagyakorlónak szigorúan pihenőnapot
+              kellene tartania ezeken a napokon – mindenki kövesse a saját
+              tanárát, és remélhetőleg, ha magasabb elvekhez hangoljuk az
               elménket, akkor boldogságot találunk a gyakorlásunkban.
-              Holdnapokon és azokon túl!”
+              Holdnapokon és azokon túl!
             </p>
             <br />
             <p>
