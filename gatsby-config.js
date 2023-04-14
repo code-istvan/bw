00e8fd12881c3d6727eac44cbc050504f1c1f3d1
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-04-14 14:47:23 +0200

    Uninstall gatsby-remark-footnotes

--- gatsby-config.js
+++ gatsby-config.js
@@ -209,24 +209,6 @@ module.exports = {
     `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-footnotes`,
-            options: {
-              footnoteBackRefPreviousElementDisplay: "inline",
-              footnoteBackRefDisplay: "inline",
-              footnoteBackRefInnerText: "^", // Defaults to: "↩"
-              //use if you want the Wikipedia style ^ link without an underline beneath it
-              footnoteBackRefAnchorStyle: `text-decoration: none;`,
-              //use "front" for Wikipedia style ^ links
-              footnoteBackRefInnerTextStartPosition: "front",
-              useFootnoteMarkerText: false, // Defaults to false
-              useCustomDivider: "<hr/><strong>References:</strong>", // Defaults to <hr/>
-            },
-          },
-        ],
-      },
     },
   ],
 }
