63eb90e9da7a9c5a1bdaca2dde3e184ae5c764b7
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-04-24 16:49:55 +0200

    Arak page text

--- src/pages/arak.js
+++ src/pages/arak.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import Layout from "../components/Layouts/Layout"
 import Seo from "../components/seo"
 import { StaticImage } from "gatsby-plugin-image"
+import "../sass/pages/_arak.scss"
 
 export default function Arak() {
   return (
@@ -32,9 +33,91 @@ export default function Arak() {
           />
         </div>
       </div>
-
-      <div className="row">
-        <h1>valami</h1>
+      <div className="arak-wrapper">
+        <div className="row mt-40px mb-20px">
+          <h3>Astanga Mysore Program</h3>
+          <table className="prices">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Árak</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Egy alkalom (drop in)</td>
+                <td>4.500.- Ft</td>
+                <td className="buy-ticket">VÁSÁRLÁS</td>
+              </tr>
+              <tr>
+                <td>Havi bérlet</td>
+                <td>34.000.- Ft</td>
+                <td className="buy-ticket">VÁSÁRLÁS</td>
+              </tr>
+              <tr>
+                <td>Éves tagság*</td>
+                <td>27.000.- Ft / hónap</td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+          <small className="footnote">
+            *Az éves tagság feltételei iránt érdeklődj a Mysore program
+            vezetőjénél
+          </small>
+        </div>
+        <br />
+        <div className="row mb-20px">
+          <h3>Vinyásza Krama Jóga</h3>
+          <table className="prices">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Árak</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tr>
+              <td>Egy alkalom (drop in)</td>
+              <td>3.500.- Ft</td>
+              <th className="buy-ticket">VÁSÁRLÁS</th>
+            </tr>
+            <tr>
+              <td>Tíz alkalmas bérlet</td>
+              <td>30.000.- Ft</td>
+              <th className="buy-ticket">VÁSÁRLÁS</th>
+            </tr>
+          </table>
+        </div>
+        <div className="row mb-20px">
+          <h3>Tanfolyamok</h3>
+          <table className="prices">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Árak</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tr>
+              <td>Kezdő astanga jóga tanfolyam</td>
+              <td>30.000.- Ft</td>
+              <th className="buy-ticket">VÁSÁRLÁS</th>
+            </tr>
+            <tr>
+              <td>Astanga mysore kurzus</td>
+              <td>30.000.- Ft</td>
+              <th className="buy-ticket">VÁSÁRLÁS</th>
+            </tr>
+          </table>
+        </div>
+        <div className="row">
+          <p>
+            A bérlet és napijegy árak <strong>nem</strong> visszatéríthetőek,{" "}
+            <strong>nem</strong> hosszabbíthatók meg, illetve{" "}
+            <strong>nem</strong> "fagyaszthatóak" be.
+          </p>
+        </div>
       </div>
     </Layout>
   )
