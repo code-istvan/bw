ec72777ecacda4b55b1ccd43fb3c6098540ec01d
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-05-04 20:19:55 +0200

    teacher card styling

--- src/components/Cards/TeachersCard.js
+++ src/components/Cards/TeachersCard.js
@@ -6,27 +6,25 @@ import "../../sass/components/_teachercard.scss"
 const TeachersCard = ({ teacherName, teacherImg, yogaStyle, title, slug }) => {
   return (
     <div className="teacher-card">
-      <div className="teacher-card__card-body">
-        <div className="row">
-          <div className="teacher-card__avatar">
-            <GatsbyImage
-              image={teacherImg}
-              alt={teacherName}
-              className="avatar__image avatar__image--big"
-            />
-          </div>
-          <div className="teacher-card__text">
-            <h3 className="teacher-card__card-name">{teacherName}</h3>
-            <p className="teacher-card__card-title clr-shades-gray">{title}</p>
-            <p className="teacher-card__card-excerpt clr-shades-gray">
-              {yogaStyle}
-            </p>
-            <CustomLink
-              link={slug}
-              title="Tovább"
-              classNames="body link-decoration-remove clr-brand-orange teacher-card__card-link"
-            />
-          </div>
+      <div className="row teacher-card__card-body">
+        <div className="teacher-card__avatar col-12-xs">
+          <GatsbyImage
+            image={teacherImg}
+            alt={teacherName}
+            className="avatar__image avatar__image--big"
+          />
+        </div>
+        <div className="teacher-card__text col-12-xs ">
+          <h3 className="teacher-card__card-name">{teacherName}</h3>
+          <p className="teacher-card__card-title clr-shades-gray">{title}</p>
+          <p className="teacher-card__card-excerpt clr-shades-gray">
+            {yogaStyle}
+          </p>
+          <CustomLink
+            link={slug}
+            title="Tovább"
+            classNames="body link-decoration-remove clr-brand-orange teacher-card__card-link"
+          />
         </div>
       </div>
     </div>
