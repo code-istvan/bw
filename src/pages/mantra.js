80c3ee7e567fd020e4db48e6e04ea72d3911851b
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-05-23 17:00:25 +0200

    tight desktop container for mantra page

--- src/pages/mantra.js
+++ src/pages/mantra.js
@@ -9,7 +9,7 @@ export default function Mantra() {
   return (
     <Layout>
       <Seo title="MANTRÁK" />
-      <div className="page-hero-container">
+      <div className="page-hero-container tight--desktop--container">
         <div className="row">
           <h1 className="page-hero-title">MANTRÁK</h1>
           <StaticImage
@@ -34,45 +34,48 @@ export default function Mantra() {
           />
         </div>
       </div>
-      <div className="row mt-40px mb-20px">
-        <h3 className="mb-20px">
-          A <i>mantra</i> szanszkrit szó, a jelentése “túljutni az elmén”.
-        </h3>
+      <div className="mantra-wrapper tight--desktop--container">
+        <div className="row mt-40px mb-20px">
+          <h3 className="mb-20px">
+            A <i>mantra</i> szanszkrit szó, a jelentése “túljutni az elmén”.
+          </h3>
 
-        <p>
-          A <i>„man”</i> jelentése elme, a <i>„tra”</i> pedig keresztül jutni,
-          átkelni. A mantrák varázsigék, amelyek képesek megváltoztatni a
-          valóságot, vagy legalábbis a mi felfogásunkat róla, ami valójában
-          jelentheti ugyanazt, mint az előbbi. De ahhoz, hogy alkalmazni tudjuk
-          ezt a varázserőt, először el kell ismernünk, hogy a mantrák
-          varázsigék, és éppen ezért csak akkor hatásosak, ha tiszta szándékkal
-          és helyes kiejtéssel alkalmazzuk őket. A legtöbbünknek nagyon sokszor
-          el kell ismételni egy mantrát ahhoz, hogy jelentkezzen a hatása. Ahogy
-          a régi korok alkimistái mondták bátorításul:
-        </p>
-        <blockquote>Az ismétlés hozza működésbe a varázslatot</blockquote>
-        <p>
-          Az ősi jógikus szövegek kijelentik, hogy Isten hang és a hang Isten:
-          Sabda Brahman. Nincs más, csak Isten. Isten a mindenség. Isten valódi.
-          Isten a valóság. Isten a hang. A valóság minden formája hang – vagyis
-          zene – mivel lényegében minden rezgésekből áll. Amit mi anyagi
-          létezésnek látunk, az valójában szemmel érzékelhető, füllel hallható
-          és a többi érzékszervünkkel is felfogható lelassult hangrezgés. A hang
-          szüli az anyagot – kezdetben volt a szó.
-        </p>
-      </div>
-      <div className="row">
-        {mantras &&
-          mantras.map(({ name, sanskrit, translation, link, chantby }) => (
-            <AccordionMantra
-              key={name}
-              title={name}
-              content={sanskrit + "<br/>" + "---" + "<br/>" + translation}
-              audio={link}
-              className="clr-shades-white accordion-matra__title"
-              chantby={chantby}
-            />
-          ))}
+          <p>
+            A <i>„man”</i> jelentése elme, a <i>„tra”</i> pedig keresztül jutni,
+            átkelni. A mantrák varázsigék, amelyek képesek megváltoztatni a
+            valóságot, vagy legalábbis a mi felfogásunkat róla, ami valójában
+            jelentheti ugyanazt, mint az előbbi. De ahhoz, hogy alkalmazni
+            tudjuk ezt a varázserőt, először el kell ismernünk, hogy a mantrák
+            varázsigék, és éppen ezért csak akkor hatásosak, ha tiszta
+            szándékkal és helyes kiejtéssel alkalmazzuk őket. A legtöbbünknek
+            nagyon sokszor el kell ismételni egy mantrát ahhoz, hogy
+            jelentkezzen a hatása. Ahogy a régi korok alkimistái mondták
+            bátorításul:
+          </p>
+          <blockquote>Az ismétlés hozza működésbe a varázslatot</blockquote>
+          <p>
+            Az ősi jógikus szövegek kijelentik, hogy Isten hang és a hang Isten:
+            Sabda Brahman. Nincs más, csak Isten. Isten a mindenség. Isten
+            valódi. Isten a valóság. Isten a hang. A valóság minden formája hang
+            – vagyis zene – mivel lényegében minden rezgésekből áll. Amit mi
+            anyagi létezésnek látunk, az valójában szemmel érzékelhető, füllel
+            hallható és a többi érzékszervünkkel is felfogható lelassult
+            hangrezgés. A hang szüli az anyagot – kezdetben volt a szó.
+          </p>
+        </div>
+        <div className="row">
+          {mantras &&
+            mantras.map(({ name, sanskrit, translation, link, chantby }) => (
+              <AccordionMantra
+                key={name}
+                title={name}
+                content={sanskrit + "<br/>" + "---" + "<br/>" + translation}
+                audio={link}
+                className="clr-shades-white accordion-matra__title"
+                chantby={chantby}
+              />
+            ))}
+        </div>
       </div>
     </Layout>
   )
