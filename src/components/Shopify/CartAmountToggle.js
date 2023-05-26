7338fc412dae733b7788053c0401b2675af760c7
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-05-26 16:02:14 +0200

    styling shopify product

--- src/components/Shopify/CartAmountToggle.js
+++ src/components/Shopify/CartAmountToggle.js
@@ -3,17 +3,19 @@ import "../../sass/components/_cartamounttoggle.scss"
 
 const CartAmountToggle = ({ amount, setDecrease, setIncrease }) => {
   return (
-    <div className="cart-button">
-      <div className="amount-toggle">
-        <button onClick={() => setDecrease()}>
-          <p>-</p>
-        </button>
-        <div className="amount-style">
-          <p>{amount}</p>
+    <div className="row">
+      <div className="cart--button">
+        <div className="amount--toggle">
+          <button onClick={() => setDecrease()}>
+            <p className="toggle--minus--button">-</p>
+          </button>
+          <div className="amount--style">
+            <p>{amount}</p>
+          </div>
+          <button onClick={() => setIncrease()}>
+            <p className="toggle--plus--button">+</p>
+          </button>
         </div>
-        <button onClick={() => setIncrease()}>
-          <p>+</p>
-        </button>
       </div>
     </div>
   )
--- src/components/Shopify/product.js
+++ src/components/Shopify/product.js
@@ -29,73 +29,84 @@ const ProductTemplate = ({ pageContext }) => {
   return (
     <Layout>
       <Seo title={product.title} />
-      <div className="row shopify--product">
-        <div className="col-12-xs col-7-md">
-          <GatsbyImage
-            image={
-              product.featuredMedia.preview.image.localFile
-                .childrenImageSharp[0].gatsbyImageData
-            }
-            alt={product.title}
-          />
-        </div>
-        <div className="col-12-xs col-5-md shopify--product--right-side">
-          <h1 className="shopify--product--title">{product.title}</h1>
-
-          <div className="row blogtags tag-button mt-20px">
-            {product.tags?.map((tag, index) => {
-              return <p key={index}>{tag}</p>
-            })}
+      <div className="shopify--product--wrapper">
+        <div className="row shopify--product--header">
+          <div className="col-12-xs col-7-md">
+            <GatsbyImage
+              image={
+                product.featuredMedia.preview.image.localFile
+                  .childrenImageSharp[0].gatsbyImageData
+              }
+              alt={product.title}
+            />
+            <div className="shopify--product--description--desktop">
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: product.descriptionHtml,
+                }}
+              />
+            </div>
           </div>
+          <div className="col-12-xs col-5-md shopify--product--right-side">
+            <h1 className="shopify--product--title">{product.title}</h1>
 
-          <div className="shopify--product--price mt-20px mb-20px">
-            <p>Ára: </p>
-            <p className="clr-brand-orange">
-              {product.priceRangeV2.maxVariantPrice.amount} Ft
-            </p>
-          </div>
+            <div className="row blogtags tag-button mt-20px">
+              {product.tags?.map((tag, index) => {
+                return <p key={index}>{tag}</p>
+              })}
+            </div>
 
-          <CartAmountToggle
-            amount={amount}
-            setDecrease={setDecrease}
-            setIncrease={setIncrease}
-          />
+            <div className="shopify--product--price mt-20px mb-40px">
+              <p>Ára: </p>
+              <p className="clr-brand-orange">
+                {product.priceRangeV2.maxVariantPrice.amount} Ft
+              </p>
+            </div>
 
-          {/* <form>
+            <CartAmountToggle
+              amount={amount}
+              setDecrease={setDecrease}
+              setIncrease={setIncrease}
+            />
+            {/* <form>
             <p>
               <label htmlFor="qty">Quantity:</label>
             </p>
             <input placeholder="1" id="qty" type="number" {...bind} />
           </form> */}
-          <div className="row mb-20px mt-20px">
-            <Button
-              type="button"
-              buttonStyle="btn--primary--outline-full"
-              onClick={() => addVariantToCart(product, amount)}
-            >
-              Hozzáadás a kosárhoz
-            </Button>
-          </div>
+            <div className="row mb-20px mt-20px">
+              <Button
+                type="button"
+                buttonStyle="btn--primary--outline-full"
+                onClick={() => addVariantToCart(product, amount)}
+              >
+                Hozzáadás a kosárhoz
+              </Button>
+            </div>
 
-          <p
-            dangerouslySetInnerHTML={{
-              __html: product.descriptionHtml,
-            }}
-          />
-
-          {/* 
+            {/* 
           <button onClick={() => addVariantToCart(product, bind.value)}>
             {" "}
             Hozzáadás a kosárhoz
           </button> */}
 
-          <ButtonIcon
-            buttonType="icon-text"
-            className="mt-40px mb-40px"
-            text="Vissza az összes termékhez"
-            icon={<Icons.ArrowLeft color="orange" />}
-            onClick={() => navigate("/shop")}
-          />
+            <ButtonIcon
+              buttonType="icon-text"
+              className="mt-40px mb-40px"
+              text="Vissza az összes termékhez"
+              icon={<Icons.ArrowLeft color="orange" />}
+              onClick={() => navigate("/shop")}
+            />
+          </div>
+        </div>
+        <div className="row shopify--product--description--mobil">
+          <div className="col-12-xs col-7-md">
+            <p
+              dangerouslySetInnerHTML={{
+                __html: product.descriptionHtml,
+              }}
+            />
+          </div>
         </div>
       </div>
     </Layout>
