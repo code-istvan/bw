f71223a20be17320ea13e45ea0cfaf3834bc2fd2
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-06-17 12:43:38 +0200

    new myore images

--- src/pages/mysore-program.js
+++ src/pages/mysore-program.js
@@ -12,7 +12,7 @@ export default function Mysoreprogram() {
           <h1 className="page-hero-title">MYSORE PROGRAM</h1>
           <StaticImage
             className="container-fluid page-hero-image page-big-image"
-            src="../images/hirlevel.jpg"
+            src="../images/mysore_desktop.jpg"
             layout="fullWidth"
             loading="eager"
             quality={95}
@@ -22,7 +22,7 @@ export default function Mysoreprogram() {
           />
           <StaticImage
             className="container-fluid page-hero-image page-mobil-image"
-            src="../images/hirlevel_mobil.jpg"
+            src="../images/mysore_mobil.jpg"
             layout="fullWidth"
             loading="eager"
             quality={95}
