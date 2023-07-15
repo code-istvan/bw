55382503db63d9e0b6d336b42f8d21860663865b
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-07-15 22:45:25 +0200

    jogairanyzatok cleaner code

--- src/pages/vinyasza-jogairanyzatok.js
+++ src/pages/vinyasza-jogairanyzatok.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import Layout from "../components/Layouts/Layout"
 import Seo from "../components/seo"
 import { StaticImage } from "gatsby-plugin-image"
