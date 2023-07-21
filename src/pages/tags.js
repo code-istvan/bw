39c2558736c07d8ebfd3d40f1368be3c37b07696
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-07-21 22:04:17 +0200

    blog tags page

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,13 +1,41 @@
 import * as React from "react"
 import Layout from "../components/Layouts/Layout"
 import Seo from "../components/seo"
+import { StaticImage } from "gatsby-plugin-image"
+import BlogTags from "../components/Blog/BlogTags"
 
 export default function Tags() {
   return (
     <Layout>
-      <Seo title="RÓLUNK" />
-      <div className="row">
-        <h1>Tags Welcome</h1>
+      <Seo title="TAGS" />
+      <div className="blog-hero-container">
+        <h1 className="blog-title">BLOG CÍMKÉK</h1>
+        <div className="row blog--desktop--container">
+          <StaticImage
+            className="container-fluid blog-hero-image hero-big-image"
+            src="../images/blog_desktop.jpg"
+            layout="fullWidth"
+            loading="eager"
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="Astanga jóga Mysore"
+            placeholder="blurred"
+          />
+          <StaticImage
+            className="container-fluid blog-hero-image hero-mobil-image"
+            src="../images/blog_mobil.jpg"
+            layout="fullWidth"
+            loading="eager"
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="Astanga jóga Mysore"
+            placeholder="blurred"
+          />
+        </div>
+      </div>
+      <div className="row mt-40px">
+        <h3 className="mb-20px">Itt találod blogbejegyzéseink címkéit</h3>
+        <BlogTags />
       </div>
     </Layout>
   )
