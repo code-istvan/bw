35c608217632f7933b5bad871eb65875f3b5f344
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-07-25 21:07:37 +0200

    accordionFaq optimalization

--- src/components/Accordions/AccordionFaq.js
+++ src/components/Accordions/AccordionFaq.js
@@ -2,43 +2,47 @@ import React, { useState, useRef } from "react"
 import Plus from "../Icons/Plus"
 import "../../sass/components/_accordionfaq.scss"
 
-function AccordionFaq(props) {
-  const [setActive, setActiveState] = useState("")
-  const [setHeight, setHeightState] = useState("0px")
-  const [setRotate, setRotateState] = useState("accordion__icon")
-  const answer = useRef(null)
+function AccordionFaq({ question, answer }) {
+  const [accordionState, setAccordionState] = useState({
+    active: false,
+    height: "0px",
+    rotate: "accordion__icon",
+  })
+
+  const answerRef = useRef(null)
 
   function toggleAccordion() {
-    setActiveState(setActive === "" ? "active" : "")
-    setHeightState(
-      setActive === "active" ? "0px" : `${answer.current.scrollHeight}px`
-    )
-    setRotateState(
-      setActive === "active"
+    setAccordionState(prevState => ({
+      active: !prevState.active,
+      height: prevState.active ? "0px" : `${answerRef.current.scrollHeight}px`,
+      rotate: prevState.active
         ? "accordion-faq__icon"
-        : "accordion-faq__icon rotate__45deg"
-    )
+        : "accordion-faq__icon rotate__45deg",
+    }))
   }
 
   return (
     <div className="accordion-faq__wrapper">
       <div className="accordion-faq__box">
         <button
-          className={`accordion-faq ${setActive}`}
+          className={`accordion-faq ${accordionState.active ? "active" : ""}`}
           onClick={toggleAccordion}
         >
           <div className="accordion-faq__plus">
-            <p className="clr-shades-white">{props.question}</p>
-            <Plus className={`${setRotate}`} fill={"hsl(37.3, 90.4%, 51.2%)"} />
+            <p className="clr-shades-white">{question}</p>
+            <Plus
+              className={`${accordionState.rotate}`}
+              fill={"hsl(37.3, 90.4%, 51.2%)"}
+            />
           </div>
         </button>
         <div
-          ref={answer}
-          style={{ maxHeight: `${setHeight}` }}
+          ref={answerRef}
+          style={{ maxHeight: `${accordionState.height}` }}
           className="accordion-faq__content"
         >
           <div className="accordion-faq__text mb-20px">
-            <p>{props.answer}</p>
+            <p>{answer}</p>
           </div>
         </div>
       </div>
