cce75850203ef9909936d34d49db90f5f49e9970
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-08-03 19:57:48 +0200

    mysore form bugfix

--- src/pages/mysore-program-jelentkezes.js
+++ src/pages/mysore-program-jelentkezes.js
@@ -16,7 +16,7 @@ export default function MysoreprogramJelentkezes() {
     <Layout>
       <Seo title="MYSORE PROGRAM" />
       <div className="page-hero-container">
-        <div className="row">
+        <div className="row tight--desktop--container">
           <h1 className="page-hero-title">MYSORE-PROGRAM JELENTKEZÉS</h1>
           <StaticImage
             className="container-fluid page-hero-image page-big-image"
@@ -40,74 +40,80 @@ export default function MysoreprogramJelentkezes() {
           />
         </div>
       </div>
-      <div className="mysore-program-jelentkezes--wrapper">
-        <div className="mt-40px mb-20px">
+      <div className="mysore-program-jelentkezes--wrapper tight--desktop--container">
+        <div className="row mt-40px mb-20px">
           <h3>JELENTKEZÉSI LAP</h3>
         </div>
-        <div className="col-12-xs col-6-md kapcsolat-uzenet">
-          <div className="row">
-            <h3 className="mb-20px">Üzenj nekünk</h3>
+        <div className="row">
+          <div className="col-12-xs col-6-md">
+            <div className="row">
+              <h3 className="mb-20px">Üzenj nekünk</h3>
+            </div>
+            <form
+              name="mysore jelentkezes"
+              action="/message-sent"
+              method="post"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+            >
+              <input
+                type="hidden"
+                name="form-name"
+                value="mysore jelentkezes"
+              />
+              <div hidden>
+                <input name="bot-field" />
+              </div>
+              <div className="row gap-five">
+                <div className="col-12-xs col-6-md">
+                  <InputField
+                    type="text"
+                    name="name"
+                    placeholder="Név"
+                    required="required"
+                  />
+                </div>
+                <div className="col-12-xs col-6-md">
+                  <InputField
+                    id="email"
+                    type="email"
+                    name="email"
+                    placeholder="E-mail"
+                    required="required"
+                    className="form-control col-12-xs col-6-md mb-20px"
+                  />
+                </div>
+              </div>
+              <div className="row massage-wrapper mb-2">
+                <TextArea
+                  className="kapcsolat__textarea"
+                  name="comments"
+                  rows="4"
+                  placeholder="Üzenet"
+                  required
+                ></TextArea>
+              </div>
+              <div className="row">
+                <div className="col contact_checkbox">
+                  <label className="form-control span span--bold clr-shades-lightGray bw-checkbox">
+                    <input type="checkbox" name="checkbox" required />
+                    <span>
+                      Megismertem és elfogadom az
+                      <Link to="/adatvedelem"> adatvédelmi tájékoztatót</Link>,
+                      hozzájárulok nevem és e-mail címem kezeléséhez.
+                    </span>
+                  </label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-12-xs col-4-md mb-2 kapcsolat-button mt-20px">
+                  <Button type="submit" buttonStyle="btn--secondary--solid">
+                    KÜLDÉS
+                  </Button>
+                </div>
+              </div>
+            </form>
           </div>
-          <form
-            name="mysore jelentkezes"
-            action="/message-sent"
-            method="post"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-          >
-            <input type="hidden" name="form-name" value="contact bandhaworks" />
-            <div hidden>
-              <input name="bot-field" />
-            </div>
-            <div className="row gap-five">
-              <div className="col-12-xs col-6-md">
-                <InputField
-                  type="text"
-                  name="name"
-                  placeholder="Név"
-                  required="required"
-                />
-              </div>
-              <div className="col-12-xs col-6-md">
-                <InputField
-                  id="email"
-                  type="email"
-                  name="email"
-                  placeholder="E-mail"
-                  required="required"
-                  className="form-control col-12-xs col-6-md mb-20px"
-                />
-              </div>
-            </div>
-            <div className="row massage-wrapper mb-2">
-              <TextArea
-                className="kapcsolat__textarea"
-                name="comments"
-                rows="4"
-                placeholder="Üzenet"
-                required
-              ></TextArea>
-            </div>
-            <div className="row">
-              <div className="col contact_checkbox">
-                <label className="form-control span span--bold clr-shades-lightGray bw-checkbox">
-                  <input type="checkbox" name="checkbox" required />
-                  <span>
-                    Megismertem és elfogadom az
-                    <Link to="/adatvedelem"> adatvédelmi tájékoztatót</Link>,
-                    hozzájárulok nevem és e-mail címem kezeléséhez.
-                  </span>
-                </label>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-12-xs col-4-md mb-2 kapcsolat-button mt-20px">
-                <Button type="submit" buttonStyle="btn--secondary--solid">
-                  KÜLDÉS
-                </Button>
-              </div>
-            </div>
-          </form>
         </div>
       </div>
     </Layout>
