a6abf333c4e92557aaee381d416e5ef9cf81cd6c
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-09-12 17:14:32 +0200

    sectin event changes

---
+++ src/components/Events/EventsFeaturedMobil.js
@@ -0,0 +1,116 @@
+import React from "react"
+import { useEventsRoll } from "../../hooks/useEventsRollQuery"
+import { Link } from "gatsby"
+import { CustomLink } from "../CustomLink"
+import { GatsbyImage } from "gatsby-plugin-image"
+import { useTeachers } from "../../hooks/useTeachersQuery"
+import "../../sass/components/_eventsRollMobil.scss"
+
+export default function EventsFeaturedMobil({
+  onlyFeatured = false,
+  showAll = false,
+  maxEventsToShow,
+  excludeTypes = [],
+}) {
+  let events = useEventsRoll()
+  const teachers = useTeachers()
+  const featuredEvents = events.filter(item => item.featured === true)
+  let filteredEvents = onlyFeatured ? featuredEvents : events
+
+  filteredEvents = filteredEvents.filter(
+    event => !excludeTypes.includes(event.esemenytipusa)
+  )
+
+  if (showAll) {
+    filteredEvents = events
+  }
+
+  // Limit the number of events to show if maxEventsToShow is defined
+  if (typeof maxEventsToShow === "number") {
+    filteredEvents = filteredEvents.slice(0, maxEventsToShow)
+  }
+
+  return (
+    <div className="mobilbaby">
+      {filteredEvents.map(
+        ({
+          title,
+          date,
+          day,
+          esemenytipusa,
+          Shortdescription,
+          teacher: teacherName,
+          eventlink,
+          // teacher: teacherLink,
+        }) => {
+          const currentTeacher = teachers.find(
+            teacher => teacher.name === teacherName
+          )
+
+          let eventFooterContent
+          if (!eventlink) {
+            eventFooterContent = <p>Részletek hamarosan</p>
+          } else {
+            eventFooterContent = (
+              <CustomLink
+                link={eventlink}
+                title="Részletek"
+                classNames="btn--third--naked-orange"
+              />
+            )
+          }
+
+          return (
+            <div className="" key={date}>
+              <div className="event-card">
+                <div className="event-card-content-wrapper">
+                  <div className="event-card-header">
+                    <h3>{title}</h3>
+                    <div className="event-card-header-secondline">
+                      <div className="col">
+                        <p className="clr-shades-gray">
+                          {date} {day}
+                        </p>
+                      </div>
+                      <div className="col">
+                        <div className="vertical-separator" />
+                      </div>
+                      <div className="col">
+                        <Link to={currentTeacher.link}>
+                          <div className="row avatar-group">
+                            <GatsbyImage
+                              image={
+                                currentTeacher.teacherimage.childImageSharp
+                                  .gatsbyImageData
+                              }
+                              alt={teacherName}
+                              className="avatar__image avatar__image--small"
+                            />
+
+                            <p className="clr-shades-gray">{teacherName}</p>
+                          </div>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="event-card-body">
+                    <p className="clr-shades-gray">{Shortdescription}</p>
+                  </div>
+                  <div
+                    className={`event-card-footer body ${
+                      eventlink === null
+                        ? "clr-brand-orange"
+                        : "custom-link-style"
+                    }`}
+                  >
+                    {eventFooterContent}
+                  </div>
+                </div>
+              </div>
+            </div>
+          )
+        }
+      )}
+    </div>
+  )
+}
--- src/components/Sections/SectionEvents.js
+++ src/components/Sections/SectionEvents.js
@@ -2,26 +2,37 @@ import React from "react"
 import Button from "../Buttons/Button"
 import { navigate } from "gatsby"
 import EventsFeaturedRoll from "../Events/EventsFeaturedRoll"
+import EventsFeaturedMobil from "../Events/EventsFeaturedMobil"
 import "../../sass/components/_sectionevents.scss"
 
 const SectionEvents = () => {
   return (
     <section className="section--events">
       <h1 className="mb-20px">Programok</h1>
-      <EventsFeaturedRoll
+
+      <EventsFeaturedMobil
         maxEventsToShow={3}
         onlyFeatured={false}
         excludeTypes={["Konferencia"]}
       />
-      <Button
-        type="button"
-        buttonStyle="btn--primary--outline"
-        onClick={() => {
-          navigate("/programok")
-        }}
-      >
-        Programok bővebben
-      </Button>
+
+      {/* <EventsFeaturedRoll
+        maxEventsToShow={3}
+        onlyFeatured={false}
+        excludeTypes={["Konferencia"]}
+      /> */}
+      <div className="mt-20px">
+        {" "}
+        <Button
+          type="button"
+          buttonStyle="btn--primary--outline"
+          onClick={() => {
+            navigate("/programok")
+          }}
+        >
+          Programok bővebben
+        </Button>
+      </div>
     </section>
   )
 }
