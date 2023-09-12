0d48f9accdd3f7dde3e9451b69e0adc80bb7a979
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-09-12 22:23:41 +0200

    new feutre to EventsFeaturedMobil

--- src/components/Events/EventsFeaturedMobil.js
+++ src/components/Events/EventsFeaturedMobil.js
@@ -30,87 +30,128 @@ export default function EventsFeaturedMobil({
     filteredEvents = filteredEvents.slice(0, maxEventsToShow)
   }
 
+  function CircleIndicator({ length, activeIndex }) {
+    return (
+      <div className="circle-indicator mt-20px mb-20px">
+        {Array.from({ length }).map((_, index) => (
+          <div
+            key={index}
+            className={`circle ${index === activeIndex ? "active" : ""}`}
+          ></div>
+        ))}
+      </div>
+    )
+  }
+
+  const [activeIndex, setActiveIndex] = React.useState(0)
+  const containerRef = React.useRef(null)
+
+  const handleScroll = () => {
+    if (!containerRef.current) return
+    const cardWidth = 300 + 12 // card width + gap
+    const scrolledWidth = containerRef.current.scrollLeft
+    const newActiveIndex = Math.round(scrolledWidth / cardWidth)
+    setActiveIndex(newActiveIndex)
+  }
+
+  React.useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.addEventListener("scroll", handleScroll)
+    }
+    return () => {
+      if (containerRef.current) {
+        containerRef.current.removeEventListener("scroll", handleScroll)
+      }
+    }
+  }, [])
+
   return (
-    <div className="mobilbaby">
-      {filteredEvents.map(
-        ({
-          title,
-          date,
-          day,
-          esemenytipusa,
-          Shortdescription,
-          teacher: teacherName,
-          eventlink,
-          // teacher: teacherLink,
-        }) => {
-          const currentTeacher = teachers.find(
-            teacher => teacher.name === teacherName
-          )
-
-          let eventFooterContent
-          if (!eventlink) {
-            eventFooterContent = <p>Részletek hamarosan</p>
-          } else {
-            eventFooterContent = (
-              <CustomLink
-                link={eventlink}
-                title="Részletek"
-                classNames="btn--third--naked-orange"
-              />
+    <>
+      <div className="mobilbaby" ref={containerRef}>
+        {filteredEvents.map(
+          ({
+            title,
+            date,
+            day,
+            esemenytipusa,
+            Shortdescription,
+            teacher: teacherName,
+            eventlink,
+            // teacher: teacherLink,
+          }) => {
+            const currentTeacher = teachers.find(
+              teacher => teacher.name === teacherName
             )
-          }
 
-          return (
-            <div className="" key={date}>
-              <div className="event-card">
-                <div className="event-card-content-wrapper">
-                  <div className="event-card-header">
-                    <h3>{title}</h3>
-                    <div className="event-card-header-secondline">
-                      <div className="col">
-                        <p className="clr-shades-gray">
-                          {date} {day}
-                        </p>
-                      </div>
-                      <div className="col">
-                        <div className="vertical-separator" />
-                      </div>
-                      <div className="col">
-                        <Link to={currentTeacher.link}>
-                          <div className="row avatar-group">
-                            <GatsbyImage
-                              image={
-                                currentTeacher.teacherimage.childImageSharp
-                                  .gatsbyImageData
-                              }
-                              alt={teacherName}
-                              className="avatar__image avatar__image--small"
-                            />
+            let eventFooterContent
+            if (!eventlink) {
+              eventFooterContent = <p>Részletek hamarosan</p>
+            } else {
+              eventFooterContent = (
+                <CustomLink
+                  link={eventlink}
+                  title="Részletek"
+                  classNames="btn--third--naked-orange"
+                />
+              )
+            }
 
-                            <p className="clr-shades-gray">{teacherName}</p>
-                          </div>
-                        </Link>
+            return (
+              <div className="" key={date}>
+                <div className="event-card">
+                  <div className="event-card-content-wrapper">
+                    <div className="event-card-header">
+                      <h3>{title}</h3>
+                      <div className="event-card-header-secondline">
+                        <div className="col">
+                          <p className="clr-shades-gray">
+                            {date} {day}
+                          </p>
+                        </div>
+                        <div className="col">
+                          <div className="vertical-separator" />
+                        </div>
+                        <div className="col">
+                          <Link to={currentTeacher.link}>
+                            <div className="row avatar-group">
+                              <GatsbyImage
+                                image={
+                                  currentTeacher.teacherimage.childImageSharp
+                                    .gatsbyImageData
+                                }
+                                alt={teacherName}
+                                className="avatar__image avatar__image--small"
+                              />
+
+                              <p className="clr-shades-gray">{teacherName}</p>
+                            </div>
+                          </Link>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="event-card-body">
-                    <p className="clr-shades-gray">{Shortdescription}</p>
-                  </div>
-                  <div
-                    className={`event-card-footer body ${
-                      eventlink === null
-                        ? "clr-brand-orange"
-                        : "custom-link-style"
-                    }`}
-                  >
-                    {eventFooterContent}
+                    <div className="event-card-body">
+                      <p className="clr-shades-gray">{Shortdescription}</p>
+                    </div>
+                    <div
+                      className={`event-card-footer body ${
+                        eventlink === null
+                          ? "clr-brand-orange"
+                          : "custom-link-style"
+                      }`}
+                    >
+                      {eventFooterContent}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          )
-        }
-      )}
-    </div>
+            )
+          }
+        )}
+      </div>
+      <CircleIndicator
+        length={filteredEvents.length}
+        activeIndex={activeIndex}
+      />
+    </>
   )
 }
