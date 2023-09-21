9a35b18a468186ed93f2b32f6a80e211924ab208
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-09-21 15:29:40 +0200

    English hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,50 +2,70 @@ import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import Button from "./Buttons/Button"
 import { navigate } from "gatsby"
+import { useLocation } from "@reach/router"
 import "../sass/components/_hero.scss"
 
-const Hero = () => (
-  <>
-    <div className="hero-container">
-      <div className="conatiner">
-        <div className="page-hero-container">
-          <div className="row">
-            <StaticImage
-              className="container-fluid page-hero-image page-big-image"
-              src="../images/hero_desktop.jpg"
-              layout="fullWidth"
-              loading="eager"
-              quality={95}
-              formats={["AUTO", "WEBP", "AVIF"]}
-              alt="Astanga jóga Mysore"
-              placeholder="blurred"
-            />
-            <StaticImage
-              className="container-fluid page-hero-image page-mobil-image"
-              src="../images/hero_mobil.jpeg"
-              layout="fullWidth"
-              loading="eager"
-              quality={95}
-              formats={["AUTO", "WEBP", "AVIF"]}
-              alt="Astanga jóga Mysore"
-              placeholder="blurred"
-            />
+const Hero = () => {
+  const location = useLocation()
+  const isEnglish = location.pathname.includes("/english")
+
+  return (
+    <>
+      <div className="hero-container">
+        <div className="conatiner">
+          <div className="page-hero-container">
+            <div className="row">
+              <StaticImage
+                className="container-fluid page-hero-image page-big-image"
+                src="../images/hero_desktop.jpg"
+                layout="fullWidth"
+                loading="eager"
+                quality={95}
+                formats={["AUTO", "WEBP", "AVIF"]}
+                alt="Astanga jóga Mysore"
+                placeholder="blurred"
+              />
+              <StaticImage
+                className="container-fluid page-hero-image page-mobil-image"
+                src="../images/hero_mobil.jpeg"
+                layout="fullWidth"
+                loading="eager"
+                quality={95}
+                formats={["AUTO", "WEBP", "AVIF"]}
+                alt="Astanga jóga Mysore"
+                placeholder="blurred"
+              />
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <div className="hero-text">
-              <p className="hero--font-big">
-                Az egyetlen kiút <br />
-                befelé vezet <br />
-              </p>
-              <p className="hero--font-subtitle mb-20px">
-                Tradicionális astanga jógaiskola a Paramaguru áldásával
-              </p>
-              <div className="hero-button-desktop">
+          <div className="row">
+            <div className="col">
+              <div className="hero-text">
+                <p className="hero--font-big">
+                  {isEnglish ? "The only way " : "Az egyetlen kiút"} <br />
+                  {isEnglish ? "OUT is IN" : "befelé vezet"} <br />
+                </p>
+                <p className="hero--font-subtitle mb-20px">
+                  {isEnglish
+                    ? "Traditional astanga yoga shala with the blessing of Paramaguru"
+                    : "Tradicionális astanga jógaiskola a Paramaguru áldásával"}
+                </p>
+                <div className="hero-button-desktop">
+                  <Button
+                    type="button"
+                    buttonStyle="btn--primary--outline"
+                    className=""
+                    onClick={() => {
+                      navigate("/astanga-mysore-program")
+                    }}
+                  >
+                    Astanga Mysore-program
+                  </Button>
+                </div>
+              </div>
+              <div className="hero--buttons">
                 <Button
                   type="button"
-                  buttonStyle="btn--primary--outline"
+                  buttonStyle="btn--primary--outline-full"
                   className=""
                   onClick={() => {
                     navigate("/astanga-mysore-program")
@@ -55,134 +75,11 @@ const Hero = () => (
                 </Button>
               </div>
             </div>
-            <div className="hero--buttons">
-              <Button
-                type="button"
-                buttonStyle="btn--primary--outline-full"
-                className=""
-                onClick={() => {
-                  navigate("/astanga-mysore-program")
-                }}
-              >
-                Astanga Mysore-program
-              </Button>
-            </div>
-          </div>
-        </div>
-
-        {/* <div className="row">
-          <div className="col">
-            <div className="hero-text">
-              <p className="hero--font-big mb-20px">
-                óh jógi, ne <br />
-                végezz ászanát <br />
-                vinyásza nélkül
-              </p>
-              <div className="row">
-                <div className="hero-risi-section">
-                  <StaticImage
-                    className="hero--risi"
-                    src="../images/risi.jpg"
-                    layout="fullWidth"
-                    loading="eager"
-                    quality={95}
-                    formats={["AUTO", "WEBP", "AVIF"]}
-                    alt="Astanga jóga Mysore"
-                    placeholder="blurred"
-                  />
-                  <div className="hero--author">
-                    <p className="hero--font-small-serif">Jóga korunta</p>
-                    <p className="hero--font-small-sans-serif">VÁMANA RISI</p>
-                  </div>
-                </div>
-                <div className="row hero-buttons hero-buttons-onthehero">
-                  <div className="col-12-xs col-6-sm mb-20px">
-                    <Button
-                      type="button"
-                      buttonStyle="btn--primary--outline"
-                      onClick={() => {
-                        navigate("/astanga-mysore-program")
-                      }}
-                    >
-                      Mysore program
-                    </Button>
-                  </div>
-                  <div className="col-12-xs col-6-sm">
-                    <Button
-                      type="button"
-                      buttonStyle="btn--primary--outline"
-                      onClick={() => {
-                        navigate("/")
-                      }}
-                    >
-                      Tanítói láncolatunk
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <StaticImage
-              className="hero_desktop"
-              src="../images/Hero_desktop.jpg"
-              layout="fullWidth"
-              loading="eager"
-              quality={95}
-              formats={["AUTO", "WEBP", "AVIF"]}
-              alt="Astanga jóga Mysore"
-              placeholder="blurred"
-            />
-            <StaticImage
-              className="hero_tablet"
-              src="../images/hero_tablet_test.jpg"
-              layout="fullWidth"
-              loading="eager"
-              quality={95}
-              formats={["AUTO", "WEBP", "AVIF"]}
-              alt="Astanga jóga Mysore"
-              placeholder="blurred"
-            />
-            <StaticImage
-              className="hero_mobil"
-              src="../images/hero_mobil.jpg"
-              layout="fullWidth"
-              loading="eager"
-              quality={95}
-              formats={["AUTO", "WEBP", "AVIF"]}
-              alt="Astanga jóga Mysore"
-              placeholder="blurred"
-            />
           </div>
         </div>
       </div>
-    </div>
-    <div className="container hero-buttons-underhero">
-      <div className="row hero-buttons">
-        <div className="col-12-xs col-6-sm mb-20px">
-          <Button
-            type="button"
-            buttonStyle="btn--primary--solid"
-            onClick={() => {
-              navigate("/astanga-mysore-program")
-            }}
-          >
-            Mysore program
-          </Button>
-        </div>
-        <div className="col-12-xs col-6-sm">
-          <Button
-            type="button"
-            buttonStyle="btn--primary--outline"
-            onClick={() => {
-              navigate("/tanfolyam")
-            }}
-          >
-            Tanfolyamok
-          </Button>
-        </div> */}
-      </div>
-    </div>
-  </>
-)
+    </>
+  )
+}
 
 export default Hero
