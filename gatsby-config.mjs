9bda5c4be45929f58594539e66ea9c1e1fc95b97
code-istvan <63305129+code-istvan@users.noreply.github.com>
2023-11-20 18:37:44 +0530

    install gatsby-plugin-google-gtag

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -17,6 +17,21 @@ const myCustomQueries = {
 
 let plugins = [
   {
+    resolve: `gatsby-plugin-google-gtag`,
+    options: {
+      // You can add multiple tracking ids and a pageview event will be fired for all of them.
+      trackingIds: [
+        "G-KYWKTRNWDQ", // Google Analytics / GA
+      ],
+      // This object is used for configuration specific to this plugin
+      pluginConfig: {
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is also optional
+      },
+    },
+  },
+  {
     resolve: `gatsby-source-filesystem`,
     options: {
       path: `./blog`,
