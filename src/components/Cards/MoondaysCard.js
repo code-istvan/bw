2d18a709d0dbd5027fab48074a4feecf4d1ddf34
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-01-02 14:12:11 +0100

    moondays 2024

--- src/components/Cards/MoondaysCard.js
+++ src/components/Cards/MoondaysCard.js
@@ -17,7 +17,7 @@ const MoondaysCard = ({
     <div className="holdnapok-card">
       <div className="holdnapok-card-content">
         <div className="holdnapok-card__header">
-          <p>2023.</p>
+          <p>2024.</p>
           <p>{month}</p>
         </div>
         <div className="holdnapok-card__body">
