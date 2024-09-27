0a36a1fd004264e66790a0f71b6d36a315433f70
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-09-27 08:16:23 +0200

    new conference date

--- src/pages/konferencia.js
+++ src/pages/konferencia.js
@@ -58,7 +58,7 @@ export default function Konferencia() {
         </div>
         <div className="konferencia-event">
           <p>
-            <strong>Következő esemény időpontja:</strong> 2024. szeptember 22.,
+            <strong>Következő esemény időpontja:</strong> 2024. október 27.,
             vasárnap, 16:00 - 18:00
           </p>
           {/* <p>
