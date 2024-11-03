a06f61079921395062416434611a5925acdb5e3f
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-11-03 09:19:04 +0100

    New akt note

--- src/pages/astanga-kezdo-tanfolyam.js
+++ src/pages/astanga-kezdo-tanfolyam.js
@@ -300,8 +300,8 @@ export default function AstangaKezdoTanfolyam() {
           }
           courseType={"tanfolyam"}
           courseTeacher={"Kisanna"}
-          courseNote={"Kevesebb mint 3 szabad hely"}
-          // courseNote={"Még 1 szabad helyünk van erre a tanfolyamra"}
+          // courseNote={"Kevesebb mint 3 szabad hely"}
+          courseNote={"Még 1 szabad helyünk van erre a tanfolyamra"}
           // courseNote={"Még 1 szabad helyünk van erre a tanfolyamra"}
         />
         <CourseApplication
